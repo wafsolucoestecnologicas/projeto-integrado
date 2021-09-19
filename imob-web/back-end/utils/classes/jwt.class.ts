543bad853cbb197b8e3c96d403f1ff83d0887518
Wellington Felix <wafsolucoestecnologicas@gmail.com>
2021-09-19 13:31:55 -0300

    Criando classe JWT.

---
+++ imob-web/back-end/utils/classes/jwt.class.ts
@@ -0,0 +1,41 @@
+import jwt from 'jsonwebtoken';
+import CONFIGURATION from '../../config/dotenv';
+
+import { Payload } from '../interfaces/jwt.interfaces';
+
+export class JWT {
+
+    private payload: Payload;
+    private expiredIn: string;
+    private privateKey: string;
+
+    constructor() {
+        this.expiredIn = '';
+        this.privateKey = (CONFIGURATION.JWT.PRIVATE_KEY) ? CONFIGURATION.JWT.PRIVATE_KEY : '';
+    }
+
+    public getPayload(): Payload {
+        return this.payload;
+    }
+
+    public setPayload(payload: Payload): void {
+        this.payload = payload;
+    }
+
+    public getExpiredIn(): string {
+        return this.expiredIn;
+    }
+
+    public setExpiredIn(expiredIn: string): void {
+        this.expiredIn = expiredIn;
+    }
+
+    public generateToken(): string {
+        return jwt.sign(this.payload, this.privateKey, { expiresIn: this.expiredIn });
+    }
+
+    public static checkToken(token: string): any {
+        return jwt.verify(token, CONFIGURATION.JWT.PRIVATE_KEY);
+    }
+
+}
