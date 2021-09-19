ba939a8ef7fa3a447404b3a2649868710c4f3b44
Wellington Felix <wafsolucoestecnologicas@gmail.com>
2021-09-19 12:23:55 -0300

    Implementando método create.

--- imob-web/back-end/src/api/controllers/user.controller.ts
+++ imob-web/back-end/src/api/controllers/user.controller.ts
@@ -1,11 +1,39 @@
+import { getRepository, Repository } from 'typeorm';
 import { Request, Response } from 'express';
 
-export class UsersController {
+import { UserModel } from '../models/user.model';
+import statusMessages from '../../../utils/utils';
+
+export class UserController {
 
     constructor() { }
 
-    public async createUser(request: Request, response: Response): Promise<Response> {
-        return response.status(200).json(request.body);
+    public async create(request: Request, response: Response): Promise<Response> {
+        try {
+            const repository: Repository<UserModel> = getRepository(UserModel);
+            const user: UserModel = new UserModel();
+
+            user.name = request.body.name.toLowerCase();
+            user.surname = request.body.surname.toLowerCase();
+            user.email = request.body.email.toLowerCase();
+            user.password = request.body.password;
+
+            const email: UserModel | undefined =
+                await repository.findOne({
+                    select: ['email'],
+                    where: {
+                        email: user.email
+                    }
+                });
+
+            if (email) {
+                return response.status(409).json({ message: statusMessages[409] });
+            }
+
+            return response.status(200).json(await repository.save(user));
+        } catch (e: any) {
+            return response.status(500).json({ message: e.message });
+        }
     }
 
 }
