8cec383b303de019439b8ec9812cc1a60d0d5293
Wellington Felix <wafsolucoestecnologicas@gmail.com>
2021-09-19 12:37:30 -0300

    Criando migrations para banco de dados, extensão, schemas e tabelas de imobiliária, perfil de usuário e usuário.

---
+++ imob-web/back-end/src/database/migrations/1632003902425-create-table-companies.ts
@@ -0,0 +1,54 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class createTableCompanies1632003902425 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                schema: 'business',
+                name: 'companies',
+                columns: [
+                    {
+                        name: 'id',
+                        type: 'integer',
+                        isPrimary: true,
+                        isGenerated: true,
+                        generationStrategy: 'increment',
+                        isUnique: true,
+                        isNullable: false
+                    },
+                    {
+                        name: 'cnpj',
+                        type: 'varchar(14)',
+                        isNullable: false
+                    },
+                    {
+                        name: 'corporate_name',
+                        type: 'varchar(256)',
+                        isNullable: false
+                    },
+                    {
+                        name: 'state_registration',
+                        type: 'varchar(30)',
+                        isNullable: false
+                    },
+                    {
+                        name: 'created_at',
+                        type: 'timestamp',
+                        isNullable: false
+                    },
+                    {
+                        name: 'updated_at',
+                        type: 'timestamp',
+                        isNullable: false
+                    }
+                ]
+            })
+        );
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable('business.companies');
+    }
+
+}
---
+++ imob-web/back-end/src/database/migrations/1632004173332-create-table-users.ts
@@ -0,0 +1,94 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class createTableUsers1632004173332 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                schema: 'authentication',
+                name: 'users',
+                columns: [
+                    {
+                        name: 'id',
+                        type: 'integer',
+                        isPrimary: true,
+                        isGenerated: true,
+                        isUnique: true,
+                        generationStrategy: 'increment',
+                        isNullable: false
+                    },
+                    {
+                        name: 'profile_id',
+                        type: 'integer',
+                        isNullable: false,
+                    },
+                    {
+                        name: 'company_id',
+                        type: 'integer',
+                        isNullable: false,
+                    },
+                    {
+                        name: 'uuid',
+                        type: 'uuid',
+                        isGenerated: true,
+                        isUnique: true,
+                        generationStrategy: 'uuid',
+                        default: 'uuid_generate_v4()',
+                        isNullable: false
+                    },
+                    {
+                        name: 'name',
+                        type: 'varchar(50)',
+                        isNullable: false
+                    },
+                    {
+                        name: 'surname',
+                        type: 'varchar(50)',
+                        isNullable: false
+                    },
+                    {
+                        name: 'email',
+                        type: 'varchar(50)',
+                        isUnique: true,
+                        isNullable: false
+                    },
+                    {
+                        name: 'password',
+                        type: 'varchar(30)',
+                        isUnique: true,
+                        isNullable: false
+                    },
+                    {
+                        name: 'created_at',
+                        type: 'timestamp',
+                        isNullable: false
+                    },
+                    {
+                        name: 'updated_at',
+                        type: 'timestamp',
+                        isNullable: false
+                    }
+                ],
+                foreignKeys: [
+                    {
+                        name: 'fk_profile_id',
+                        columnNames: ['profile_id'],
+                        referencedTableName: 'authentication.profiles',
+                        referencedColumnNames: ['id']
+                    },
+                    {
+                        name: 'fk_company_id',
+                        columnNames: ['company_id'],
+                        referencedTableName: 'business.companies',
+                        referencedColumnNames: ['id']
+                    }
+                ]
+            })
+        );
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable('authentication.users');
+    }
+
+}
