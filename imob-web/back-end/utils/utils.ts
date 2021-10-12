bbbfdf9f8e856900c8cde6008ea5d7e0f67b5531
Wellington Felix <wafsolucoestecnologicas@gmail.com>
2021-10-12 19:34:50 -0300

    Adicionando novas mensagens no objeto statusMessage e exportando o array returnMessages.

--- imob-web/back-end/utils/utils.ts
+++ imob-web/back-end/utils/utils.ts
@@ -1,6 +1,16 @@
-const statusMessages: { [key: number]: string } = {
+export const statusMessages: { [key: number]: string } = {
+    400: 'O pedido não pôde ser entregue devido à sintaxe incorreta!',
     401: 'Não possui credenciais de autenticação válidas para o recurso!',
-    409: 'Solicitação atual conflitou com o recurso que está no servidor!'
+    409: 'Solicitação atual conflitou com o recurso que está no servidor!',
+    500: 'Erro no servidor ao processar a solicitação!'
 };
 
-export default statusMessages;
+export const returnMessages: string[] = [
+    'Dados do corpo da requisição inválidos.',
+    'Registro não existe na base de dados.',
+    'É necessário o id do registro.',
+    'Já existe um registro com o CNPJ informado no banco de dados.',
+    'Já existe um registro com o e-mail informado no banco de dados.',
+    'Erro interno ao cadastrar uma nova imobiliária.',
+    'Erro interno ao recuperar o perfil de usuário gestor.'
+];
