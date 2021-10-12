d4cc530c9e221530392d9f10c6672b51958337f7
Wellington Felix <wafsolucoestecnologicas@gmail.com>
2021-10-12 19:40:02 -0300

    Adicionando rotas de imobiliária e perfil.

--- imob-web/back-end/src/routes/index.ts
+++ imob-web/back-end/src/routes/index.ts
@@ -1,8 +1,12 @@
 import authentication from './authentication.routes';
+import company from './company.routes';
+import profile from './profile.routes';
 import user from './user.routes';
 
 const routes: any = {
     authentication,
+    company,
+    profile,
     user
 };
 
