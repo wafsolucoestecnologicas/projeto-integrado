2f55e8d05347e102677caa8a666ca15503b932b3
Wellington Felix <wafsolucoestecnologicas@gmail.com>
2021-10-12 20:01:29 -0300

    Refatorando middleware.

--- imob-web/back-end/src/middlewares/authentication.middleware.ts
+++ imob-web/back-end/src/middlewares/authentication.middleware.ts
@@ -1,23 +1,44 @@
-import { NextFunction, Request, Response } from "express";
+import { Request, Response, NextFunction } from 'express';
+import { JWT } from '../../utils/classes/jwt.class';
+import { Payload } from '../../utils/interfaces/jwt.interfaces';
+import { statusMessages } from '../../utils/utils';
 
-import { JWT } from "../../utils/classes/jwt.class";
-import { Payload } from "../../utils/interfaces/jwt.interfaces";
-import statusMessages from "../../utils/utils";
+const validateURLWithoutAuthentication = (url: string): boolean => {
+    url = url.split('/')[1];
 
-export function authenticationMiddleware(request: Request, response: Response, next: NextFunction) {
-    if (!request.headers.authorization) {
-        return response.status(401).json({ message: statusMessages[401] });
-    }
+    /**@TODO Deixar apenas a rota de users futuramente */
+    const urls: string[] = [
+        'users',
+        'companies',
+        'profiles'
+    ];
 
-    const token: string = request.headers.authorization.replace('Bearer', '').trim();
+    const isValid: boolean = urls.includes(url);
 
+    return isValid;
+};
+
+const authenticationMiddleware = (request: Request, response: Response, next: NextFunction): Response | void => {
     try {
-        const data: Payload = JWT.checkToken(token);
-        
-        request.payload = data;
+        const result: boolean =
+            validateURLWithoutAuthentication(request.url);
 
-        next();
+        if (result) {
+            next();
+        } else if (!request.headers.authorization) {
+            return response.status(401).json({ message: statusMessages[401] });
+        } else {
+            const token: string = request.headers.authorization.replace('Bearer', '').trim();
+
+            const data: Payload = JWT.checkToken(token);
+
+            request.payload = data;
+
+            next();
+        }
     } catch (e: any) {
         return response.status(500).json({ message: e.message });
     }
-}
+};
+
+export default authenticationMiddleware;
