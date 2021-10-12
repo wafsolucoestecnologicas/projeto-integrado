d58e7710847336877ed11cb05f5b7dbd462df2fe
Wellington Felix <wafsolucoestecnologicas@gmail.com>
2021-10-12 19:39:26 -0300

    Adicionando novas rotas.

--- imob-web/back-end/src/routes/user.routes.ts
+++ imob-web/back-end/src/routes/user.routes.ts
@@ -4,6 +4,10 @@ import { UserController } from '../api/controllers/user.controller';
 const userController: UserController = new UserController();
 const router: Router = Router();
 
+router.get('/', userController.index);
 router.post('/', userController.create);
+router.get('/:id', userController.read);
+router.put('/:id', userController.update);
+router.delete('/:id', userController.delete);
 
 export default router;
