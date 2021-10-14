6c0a42bf86d568f563fd21775fb16f6a122931f3
Wellington Felix <wafsolucoestecnologicas@gmail.com>
2021-10-14 06:11:58 -0300

    Refatorando código.

--- imob-web/back-end/src/api/controllers/authentication.controller.ts
+++ imob-web/back-end/src/api/controllers/authentication.controller.ts
@@ -1,9 +1,8 @@
-import { getRepository, Repository } from 'typeorm';
 import { Request, Response } from 'express';
 import { UserModel } from '../models/user.model';
 import { JWT } from '../../../utils/classes/jwt.class';
-import { statusMessages } from '../../../utils/utils';
-import bcrypt from 'bcryptjs';
+import { returnMessages, statusMessages } from '../../../utils/utils';
+import { UserService } from '../services/user.service';
 
 export class AuthenticationController {
 
@@ -11,33 +10,51 @@ export class AuthenticationController {
 
     public async authenticate(request: Request, response: Response): Promise<Response> {
         try {
-            const repository: Repository<UserModel> = getRepository(UserModel);
+            const userService: UserService =
+                new UserService();
 
-            const user: UserModel | undefined =
-                await repository.findOne({
-                    where: {
-                        email: request.body.email
+            if (request.body.email && request.body.password) {
+                const result: boolean =
+                    await userService.alreadyRegisteredByEmail(request.body.email);
+
+                if (!result) {
+                    return response.status(401).json({ message: statusMessages[401] });
+                }
+
+                const userModel: UserModel | undefined =
+                    await userService.findByEmail(request.body.email);
+
+                if (userModel) {
+                    const isValid: boolean =
+                        await userService.validatePassword(userModel, request.body.password);
+
+                    if (!isValid) {
+                        return response.status(400).json({ message: `${statusMessages[400]} ${returnMessages[7]}` });
                     }
-                });
 
-            if (!user) {
-                return response.status(401).json({ message: statusMessages[401] });
+                    userModel.password = '';
+
+                    const jwt: JWT = new JWT();
+
+                    jwt.setPayload({
+                        id: userModel.id,
+                        uuid: userModel.uuid,
+                        name: userModel.name,
+                        surname: userModel.surname,
+                        email: userModel.email,
+                        isAdmin: userModel.profile.isAdmin,
+                        permissions: userModel.profile.permissions
+                    });
+
+                    jwt.setExpiredIn('1d');
+
+                    return response.status(200).json({ userModel, token: jwt.generateToken() });
+                } else {
+                    return response.status(500).json({ message: `${statusMessages[500]} ${returnMessages[1]}` });
+                }
+            } else {
+                return response.status(400).json({ message: `${statusMessages[400]} ${returnMessages[0]}` });
             }
-
-            const isValidPassword: boolean = await bcrypt.compare(request.body.password, user.password);
-
-            if (!isValidPassword) {
-                return response.status(401).json({ message: statusMessages[401] });
-            }
-
-            user.password = '';
-
-            const jwt: JWT = new JWT();
-
-            jwt.setPayload({ id: user.id });
-            jwt.setExpiredIn('1d');
-
-            return response.status(200).json({ user, token: jwt.generateToken() });
         } catch (error: any) {
             return response.status(500).json({ message: error.message });
         }
