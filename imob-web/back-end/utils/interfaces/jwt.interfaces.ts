e75d9305c3c98488880a41f9612643ff2599d4b9
Wellington Felix <wafsolucoestecnologicas@gmail.com>
2021-10-14 06:14:49 -0300

    Alterando interface Payload.

--- imob-web/back-end/utils/interfaces/jwt.interfaces.ts
+++ imob-web/back-end/utils/interfaces/jwt.interfaces.ts
@@ -1,5 +1,11 @@
 export interface Payload {
     id: number;
+    uuid: string;
+    name: string;
+    surname: string;
+    email: string;
+    isAdmin: boolean;
+    permissions: JSON;
     iat?: number;
     exp?: number;
 }
