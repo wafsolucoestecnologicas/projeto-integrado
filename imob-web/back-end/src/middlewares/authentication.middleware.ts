c3611a9131eb08a7b3a85c3c0917272636ddae9c
Wellington Felix <wafsolucoestecnologicas@gmail.com>
2021-10-14 06:14:19 -0300

    Adicionando rota de autenticação como URL sem autenticação.

--- imob-web/back-end/src/middlewares/authentication.middleware.ts
+++ imob-web/back-end/src/middlewares/authentication.middleware.ts
@@ -6,8 +6,9 @@ import { statusMessages } from '../../utils/utils';
 const validateURLWithoutAuthentication = (url: string): boolean => {
     url = url.split('/')[1];
 
-    /**@TODO Deixar apenas a rota de users futuramente */
+    /**@TODO Deixar apenas as rotas de authentication e users futuramente */
     const urls: string[] = [
+        'authentication',
         'users',
         'companies',
         'profiles'
