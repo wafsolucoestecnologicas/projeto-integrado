02b5e5970c6b46fa115bf5baae35853af216baaf
Wellington Felix <wafsolucoestecnologicas@gmail.com>
2021-10-23 20:33:32 -0300

    Exportando rotas de pessoas.

--- imob-web/back-end/src/routes/index.ts
+++ imob-web/back-end/src/routes/index.ts
@@ -2,12 +2,26 @@ import authentication from './authentication.routes';
 import company from './company.routes';
 import profile from './profile.routes';
 import user from './user.routes';
+import administrator from './administrator.routes';
+import manager from './manager.routes';
+import advisor from './advisor.routes';
+import broker from './broker.routes';
+import secretary from './secretary.routes';
+import owner from './owner.routes';
+import customer from './customer.routes';
 
 const routes: any = {
     authentication,
     company,
     profile,
-    user
+    user,
+    administrator,
+    manager,
+    advisor,
+    broker,
+    secretary,
+    owner,
+    customer
 };
 
 export default routes;
