14950eb74023d4250ee33911f1ed2f13ff11384c
Wellington Felix <wafsolucoestecnologicas@gmail.com>
2021-10-30 11:38:13 -0300

    Adicionando novas propriedades na interface.

--- imob-web/back-end/src/api/models/user.model.ts
+++ imob-web/back-end/src/api/models/user.model.ts
@@ -1,5 +1,10 @@
-import { CompanyEntity } from "../entities/company.entity";
-import { ProfileEntity } from "../entities/profile.entity";
+import { CompanyEntity } from '../entities/company.entity';
+import { ProfileEntity } from '../entities/profile.entity';
+import { AdministratorEntity } from '../entities/administrator.entity';
+import { ManagerEntity } from '../entities/manager.entity';
+import { AdvisorEntity } from '../entities/advisor.entity';
+import { BrokerEntity } from '../entities/broker.entity';
+import { SecretaryEntity } from '../entities/secretary.entity';
 
 export interface UserModel {
     id: number;
@@ -8,11 +13,21 @@ export interface UserModel {
     surname: string;
     email: string;
     password: string;
+    isAdministrator: boolean;
+    isManager: boolean;
+    isAdvisor: boolean;
+    isBroker: boolean;
+    isSecretary: boolean;
     createdAt: Date;
     updatedAt: Date;
     company: CompanyEntity;
     profile: ProfileEntity;
-    
+    administrator?: AdministratorEntity;
+    manager?: ManagerEntity;
+    advisor?: AdvisorEntity;
+    broker?: BrokerEntity;
+    secretary?: SecretaryEntity;
+
     encryptPassword(): void;
     setCreatedAt(): void;
     setUpdatedAt(): void;
