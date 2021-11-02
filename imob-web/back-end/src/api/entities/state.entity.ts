281e00eb91b2f11ed0bc1ee35a5c8cc3006c610e
Wellington Felix <wafsolucoestecnologicas@gmail.com>
2021-11-02 16:50:49 -0300

    Criando entidade de estado.

---
+++ imob-web/back-end/src/api/entities/state.entity.ts
@@ -0,0 +1,31 @@
+import { Entity, Column } from 'typeorm';
+import { StateModel } from '../models/state.model';
+
+@Entity({
+    schema: 'public',
+    name: 'states'
+})
+export class StateEntity implements StateModel {
+
+    @Column({
+        name: 'id',
+        type: 'int',
+        primary: true,
+        generated: 'increment',
+        unique: true,
+        nullable: false,
+        comment: 'Código sequencial único do estado'
+    })
+    public id: number;
+
+    @Column({
+        name: 'state',
+        type: 'text',
+        nullable: false,
+        comment: 'Nome do estado'
+    })
+    public state: string;
+
+    constructor() { }
+
+}
