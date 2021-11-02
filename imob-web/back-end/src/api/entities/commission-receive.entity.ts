2d61eef0b42f64e12e1e642f48b7cfea4dcd5960
Wellington Felix <wafsolucoestecnologicas@gmail.com>
2021-11-02 16:52:41 -0300

    Criando entidade de comissão à receber.

---
+++ imob-web/back-end/src/api/entities/commission-receive.entity.ts
@@ -0,0 +1,79 @@
+import { Entity, Column, BeforeInsert, BeforeUpdate, OneToOne, JoinColumn } from 'typeorm';
+import { CommissionReceiveModel } from '../models/commission-receive.model';
+import { CompanyEntity } from './company.entity';
+import { PropertyEntity } from './property.entity';
+
+@Entity({
+    schema: 'commission',
+    name: 'commissions_receive'
+})
+export class CommissionReceiveEntity implements CommissionReceiveModel {
+
+    @Column({
+        name: 'id',
+        type: 'int',
+        primary: true,
+        generated: 'increment',
+        unique: true,
+        nullable: false,
+        comment: 'Código sequencial único da comissão à receber'
+    })
+    public id: number;
+
+    @Column({
+        name: 'date',
+        type: 'date',
+        nullable: false,
+        comment: 'Data da venda do imóvel que garante a comissão à receber'
+    })
+    public date: Date;
+
+    @Column({
+        name: 'value',
+        type: 'numeric',
+        nullable: false,
+        comment: 'Valor da comissão à receber'
+    })
+    public value: number;
+
+    @Column({
+        name: 'created_at',
+        type: 'timestamp',
+        nullable: false,
+        default: () => 'CURRENT_TIMESTAMP(6)',
+        comment: 'Data de criação do registro',
+    })
+    public createdAt: Date;
+
+    @Column({
+        name: 'updated_at',
+        type: 'timestamp',
+        nullable: false,
+        default: () => 'CURRENT_TIMESTAMP(6)',
+        onUpdate: 'CURRENT_TIMESTAMP(6)',
+        comment: 'Data de atualização do registro',
+    })
+    public updatedAt: Date;
+
+    @OneToOne(() => CompanyEntity)
+    @JoinColumn({ name: 'company_id' })
+    public company: CompanyEntity;
+
+    @OneToOne(() => PropertyEntity)
+    @JoinColumn({ name: 'property_id' })
+    public property: PropertyEntity;
+
+    constructor() { }
+
+    @BeforeInsert()
+    public setCreatedAt(): void {
+        this.createdAt = new Date();
+    }
+
+    @BeforeInsert()
+    @BeforeUpdate()
+    public setUpdatedAt(): void {
+        this.updatedAt = new Date();
+    }
+
+}
