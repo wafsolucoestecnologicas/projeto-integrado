2a74c649f152f117f843844891e9714b0ff7774d
Wellington Felix <wafsolucoestecnologicas@gmail.com>
2021-11-02 16:51:19 -0300

    Criando entidade de bairro.

---
+++ imob-web/back-end/src/api/entities/neighborhood.entity.ts
@@ -0,0 +1,36 @@
+import { Entity, Column, OneToOne, JoinColumn } from 'typeorm';
+import { NeighborhoodModel } from '../models/neighborhood.model';
+import { CityEntity } from './city.entity';
+
+@Entity({
+    schema: 'public',
+    name: 'neighborhoods'
+})
+export class NeighborhoodEntity implements NeighborhoodModel {
+
+    @Column({
+        name: 'id',
+        type: 'int',
+        primary: true,
+        generated: 'increment',
+        unique: true,
+        nullable: false,
+        comment: 'Código sequencial único do bairro'
+    })
+    public id: number;
+
+    @Column({
+        name: 'neighborhood',
+        type: 'text',
+        nullable: false,
+        comment: 'Nome do bairro'
+    })
+    public neighborhood: string;
+
+    @OneToOne(() => CityEntity)
+    @JoinColumn({ name: 'city_id' })
+    public city: CityEntity;
+
+    constructor() { }
+
+}
