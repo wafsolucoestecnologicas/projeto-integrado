a3a7b18acbac864fee893ba318b5a621982e98e7
Wellington Felix <wafsolucoestecnologicas@gmail.com>
2021-11-02 16:50:25 -0300

    Criando interface de negócio.

---
+++ imob-web/back-end/src/api/models/business.model.ts
@@ -0,0 +1,58 @@
+import { CompanyEntity } from '../entities/company.entity';
+import { AdministratorEntity } from '../entities/administrator.entity';
+import { ManagerEntity } from '../entities/manager.entity';
+import { AdvisorEntity } from '../entities/advisor.entity';
+import { BrokerEntity } from '../entities/broker.entity';
+import { SecretaryEntity } from '../entities/secretary.entity';
+import { OwnerEntity } from '../entities/owner.entity';
+import { CustomerEntity } from '../entities/customer.entity';
+import { PropertyEntity } from '../entities/property.entity';
+import { LeadEntity } from '../entities/lead.entity';
+
+export interface BusinessModel {
+    id: number;
+    status: number;
+    dateVisit: Date;
+    dateSale: Date;
+    visitForm: string;
+    propertyRegistration: string;
+    propertySaleContract: string;
+    IBTI: string;
+    customerRG: string;
+    customerCPF: string;
+    customerAddressProof: string;
+    customerPayslip: string;
+    ownerRG: string;
+    ownerCPF: string;
+    ownerAddressProof: string;
+    ownerPayslip: string;
+    createdByAdministrator: boolean;
+    createdByManager: boolean;
+    createdBySecretary: boolean;
+    redirectedManagerId: number;
+    redirectedAdvisorId: number;
+    redirectedBrokerId: number;
+    createdAt: Date;
+    updatedAt: Date;
+    company: CompanyEntity;
+    administrator?: AdministratorEntity;
+    manager?: ManagerEntity;
+    advisor?: AdvisorEntity;
+    broker?: BrokerEntity;
+    secretary?: SecretaryEntity;
+    owner: OwnerEntity;
+    customer: CustomerEntity;
+    property: PropertyEntity;
+    lead: LeadEntity;
+
+    setCreatedAt(): void;
+    setUpdatedAt(): void;
+}
+
+export const status: string[] = [
+    'Prospecção',
+    'Visita',
+    'Proposta',
+    'Rejeitado',
+    'Fechado'
+];
