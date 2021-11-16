9f0f19d209bd72f3659d2e98811dbca4764446b0
Wellington Felix <wafsolucoestecnologicas@gmail.com>
2021-11-16 18:39:29 -0300

    Criando arquivo de rotas de comissões a pagar.

---
+++ imob-web/back-end/src/routes/commission-payable.routes.ts
@@ -0,0 +1,13 @@
+import { Router } from 'express';
+import { CommissionPayableController } from '../api/controllers/commission-payable.controller';
+
+const commissionPayableController: CommissionPayableController = new CommissionPayableController();
+const router: Router = Router();
+
+router.get('/', commissionPayableController.index);
+router.post('/', commissionPayableController.create);
+router.get('/:id', commissionPayableController.read);
+router.put('/:id', commissionPayableController.update);
+router.delete('/:id', commissionPayableController.delete);
+
+export default router;
