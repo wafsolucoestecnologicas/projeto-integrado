db0bfab62d3f3b6f063bffa198e072e63561c297
Wellington Felix <wafsolucoestecnologicas@gmail.com>
2021-11-19 09:47:18 -0300

    Alterando rota de busca de endereço para receber uma query string.

--- imob-web/back-end/src/routes/address.routes.ts
+++ imob-web/back-end/src/routes/address.routes.ts
@@ -4,11 +4,11 @@ import { AddressController } from '../api/controllers/address.controller';
 const addressController: AddressController = new AddressController();
 const router: Router = Router();
 
+router.get('/search', addressController.searchAddressInAPIViaCEP);
 router.get('/', addressController.index);
 router.post('/', addressController.create);
 router.get('/:id', addressController.read);
 router.put('/:id', addressController.update);
 router.delete('/:id', addressController.delete);
-router.get('/search/:cep', addressController.searchAddressInAPIViaCEP);
 
 export default router;
