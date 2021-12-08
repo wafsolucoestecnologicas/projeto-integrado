b32b61d676b8950dc896fb5c4c4bb789178890eb
Wellington Felix <wafsolucoestecnologicas@gmail.com>
2021-12-08 00:06:33 -0300

    Adicionando entidades na interface do payload.

--- imob-web/back-end/utils/interfaces/jwt.interfaces.ts
+++ imob-web/back-end/utils/interfaces/jwt.interfaces.ts
@@ -1,4 +1,9 @@
-import { CompanyEntity } from "../../src/api/entities/company.entity";
+import { CompanyEntity } from '../../src/api/entities/company.entity';
+import { AdministratorEntity } from '../../src/api/entities/administrator.entity';
+import { ManagerEntity } from '../../src/api/entities/manager.entity';
+import { AdvisorEntity } from '../../src/api/entities/advisor.entity';
+import { BrokerEntity } from '../../src/api/entities/broker.entity';
+import { SecretaryEntity } from '../../src/api/entities/secretary.entity';
 
 export interface Payload {
     id: number;
@@ -9,6 +14,11 @@ export interface Payload {
     email: string;
     isAdmin: boolean;
     permissions: JSON;
+    administrator?: AdministratorEntity;
+    manager?: ManagerEntity;
+    advisor?: AdvisorEntity;
+    broker?: BrokerEntity;
+    secretary?: SecretaryEntity;
     iat?: number;
     exp?: number;
 }
