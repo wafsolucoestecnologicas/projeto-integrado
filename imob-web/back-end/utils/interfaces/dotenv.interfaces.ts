78f8bae55592d6e5907910a31b8afd1262a50511
Wellington Felix <wafsolucoestecnologicas@gmail.com>
2021-12-18 19:19:25 -0300

    Adicionando propriedades na interface.

--- imob-web/back-end/utils/interfaces/dotenv.interfaces.ts
+++ imob-web/back-end/utils/interfaces/dotenv.interfaces.ts
@@ -1,8 +1,15 @@
 export interface Configuration {
-    ENVIRONMENT: string,
+    ENVIRONMENT: string;
     SERVER: {
         PORT: number
     },
+    DATABASE: {
+        NAME: string,
+        HOST: string,
+        PORT: number,
+        USERNAME: string,
+        PASSWORD: string
+    }
     JWT: {
         PRIVATE_KEY: string
     }
