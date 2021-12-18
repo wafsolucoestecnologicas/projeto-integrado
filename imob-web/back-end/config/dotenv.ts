2ae624569334b68d46df382410824ac2f511e781
Wellington Felix <wafsolucoestecnologicas@gmail.com>
2021-12-18 19:18:33 -0300

    Implementando código para recuperar os dados de conexão com o bando de dados no heroku.

--- imob-web/back-end/config/dotenv.ts
+++ imob-web/back-end/config/dotenv.ts
@@ -17,17 +17,45 @@ switch (process.env.NODE_ENV) {
 
 dotenv.config({ path: pathConfig });
 
+/**
+ * Montando objeto com os dados de conexão do banco de dados no Heroku
+ */
+const url: string | undefined = process.env.DATABASE_URL;
+const obj: any = {};
+
+if (url) {
+    const array: string[] = url.split(' ');
+
+    obj.name = array[0].slice(7);
+    obj.host = array[1].slice(5);
+    obj.port = array[2].slice(5);
+    obj.user = array[3].slice(5);
+    obj.password = array[4].slice(9);
+}
+
 const CONFIGURATION: Configuration = {
     ENVIRONMENT: process.env.NODE_ENV || '',
     SERVER: {
         PORT: 0
+    },
+    DATABASE: {
+        NAME: '',
+        HOST: '',
+        PORT: 0,
+        USERNAME: '',
+        PASSWORD: ''
     },
     JWT: {
         PRIVATE_KEY: ''
     }
 };
 
-CONFIGURATION.SERVER.PORT = Number(process.env.SERVER_PORT);
+CONFIGURATION.SERVER.PORT = Number(process.env.PORT);
+CONFIGURATION.DATABASE.NAME = obj.name;
+CONFIGURATION.DATABASE.HOST = obj.host;
+CONFIGURATION.DATABASE.PORT = obj.port;
+CONFIGURATION.DATABASE.USERNAME = obj.username;
+CONFIGURATION.DATABASE.PASSWORD = obj.password;
 CONFIGURATION.JWT.PRIVATE_KEY = process.env.PRIVATE_KEY || '';
 
 export default CONFIGURATION;
