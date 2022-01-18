88f492ee294c0463dd663f62596dc9884af3463d
Wellington Felix <wafsolucoestecnologicas@gmail.com>
2022-01-18 20:04:10 -0300

    Melhorando perfomance da criação de negócios.

--- imob-web/back-end/src/api/controllers/business.controller.ts
+++ imob-web/back-end/src/api/controllers/business.controller.ts
@@ -2,6 +2,24 @@ import { DeleteResult, EntityManager, getManager, UpdateResult } from 'typeorm';
 import { Request, Response } from 'express';
 import { BusinessEntity } from '../entities/business.entity';
 import { BusinessService } from '../services/business.service';
+import { AdministratorEntity } from '../entities/administrator.entity';
+import { AdministratorService } from '../services/administrator.service';
+import { ManagerEntity } from '../entities/manager.entity';
+import { ManagerService } from '../services/manager.service';
+import { AdvisorEntity } from '../entities/advisor.entity';
+import { AdvisorService } from '../services/advisor.service';
+import { BrokerEntity } from '../entities/broker.entity';
+import { BrokerService } from '../services/broker.service';
+import { SecretaryEntity } from '../entities/secretary.entity';
+import { SecretaryService } from '../services/secretary.service';
+import { OwnerEntity } from '../entities/owner.entity';
+import { OwnerService } from '../services/owner.service';
+import { CustomerEntity } from '../entities/customer.entity';
+import { CustomerService } from '../services/customer.service';
+import { PropertyEntity } from '../entities/property.entity';
+import { PropertyService } from '../services/property.service';
+import { LeadEntity } from '../entities/lead.entity';
+import { LeadService } from '../services/lead.service';
 import { returnMessages, statusMessages } from '../../../utils/utils';
 
 export class BusinessController {
@@ -32,7 +50,136 @@ export class BusinessController {
                     businessService.validateData(request.body);
 
                 if (result) {
+                    const administratorService: AdministratorService = new AdministratorService();
+                    const managerService: ManagerService = new ManagerService();
+                    const advisorService: AdvisorService = new AdvisorService();
+                    const brokerService: BrokerService = new BrokerService();
+                    const secretaryService: SecretaryService = new SecretaryService();
+                    const ownerService: OwnerService = new OwnerService();
+                    const customerService: CustomerService = new CustomerService();
+                    const propertyService: PropertyService = new PropertyService();
+                    const leadService: LeadService = new LeadService();
+
                     request.body.company = request.payload.company;
+                    request.body.dateVisit = (request.body.dateVisit) ? request.body.dateVisit : null;
+                    request.body.dateSale = (request.body.dateSale) ? request.body.dateSale : null;
+
+                    if (request.body.administrator && request.body.administrator.id) {
+                        const administratorEntity: AdministratorEntity | undefined =
+                            await administratorService.read(request.body.administrador.id);
+
+                        if (administratorEntity) {
+                            request.body.administrador = administratorEntity
+                        } else {
+                            request.body.administrator = null;
+                        }
+                    } else {
+                        request.body.administrator = null;
+                    }
+
+                    if (request.body.manager && request.body.manager.id) {
+                        const managerEntity: ManagerEntity | undefined =
+                            await managerService.read(request.body.manager.id);
+
+                        if (managerEntity) {
+                            request.body.manager = managerEntity;
+                        } else {
+                            request.body.manager = null;
+                        }
+                    } else {
+                        request.body.manager = null;
+                    }
+
+                    if (request.body.advisor && request.body.advisor.id) {
+                        const advisorEntity: AdvisorEntity | undefined =
+                            await advisorService.read(request.body.advisor.id);
+
+                        if (advisorEntity) {
+                            request.body.advisor = advisorEntity;
+                        } else {
+                            request.body.advisor = null;
+                        }
+                    } else {
+                        request.body.advisor = null;
+                    }
+
+                    if (request.body.broker && request.body.broker.id) {
+                        const brokerEntity: BrokerEntity | undefined =
+                            await brokerService.read(request.body.broker.id);
+
+                        if (brokerEntity) {
+                            request.body.broker = brokerEntity;
+                        } else {
+                            request.body.broker = null;
+                        }
+                    } else {
+                        request.body.broker = null;
+                    }
+
+                    if (request.body.secretary && request.body.secretary.id) {
+                        const secretaryEntity: SecretaryEntity | undefined =
+                            await secretaryService.read(request.body.secretary.id);
+
+                        if (secretaryEntity) {
+                            request.body.secretary = secretaryEntity;
+                        } else {
+                            request.body.secretary = null;
+                        }
+                    } else {
+                        request.body.secretary = null;
+                    }
+
+                    if (request.body.owner && request.body.owner.id) {
+                        const ownerEntity: OwnerEntity | undefined =
+                            await ownerService.read(request.body.owner.id);
+
+                        if (ownerEntity) {
+                            request.body.owner = ownerEntity;
+                        } else {
+                            request.body.owner = null;
+                        }
+                    } else {
+                        request.body.owner = null;
+                    }
+
+                    if (request.body.customer && request.body.customer.id) {
+                        const customerEntity: CustomerEntity | undefined =
+                            await customerService.read(request.body.customer.id);
+
+                        if (customerEntity) {
+                            request.body.customer = customerEntity;
+                        } else {
+                            request.body.customer = null;
+                        }
+                    } else {
+                        request.body.customer = null;
+                    }
+
+                    if (request.body.property && request.body.property.id) {
+                        const propertyEntity: PropertyEntity | undefined =
+                            await propertyService.read(request.body.property.id);
+
+                        if (propertyEntity) {
+                            request.body.property = propertyEntity;
+                        } else {
+                            request.body.property = null;
+                        }
+                    } else {
+                        request.body.property = null;
+                    }
+
+                    if (request.body.lead && request.body.lead.id) {
+                        const leadEntity: LeadEntity | undefined =
+                            await leadService.read(request.body.lead.id);
+
+                        if (leadEntity) {
+                            request.body.lead = leadEntity;
+                        } else {
+                            request.body.lead = null;
+                        }
+                    } else {
+                        request.body.lead = null;
+                    }
 
                     const businessEntity: BusinessEntity =
                         await businessService.create(request.body, transaction);
