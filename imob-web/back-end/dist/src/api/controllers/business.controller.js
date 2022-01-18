02ba681fcca06c46be93c377042cbf3a603547c8
Wellington Felix <wafsolucoestecnologicas@gmail.com>
2022-01-18 20:11:12 -0300

    Gerando versão para produção.

--- imob-web/back-end/dist/src/api/controllers/business.controller.js
+++ imob-web/back-end/dist/src/api/controllers/business.controller.js
@@ -39,6 +39,15 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.BusinessController = void 0;
 var typeorm_1 = require("typeorm");
 var business_service_1 = require("../services/business.service");
+var administrator_service_1 = require("../services/administrator.service");
+var manager_service_1 = require("../services/manager.service");
+var advisor_service_1 = require("../services/advisor.service");
+var broker_service_1 = require("../services/broker.service");
+var secretary_service_1 = require("../services/secretary.service");
+var owner_service_1 = require("../services/owner.service");
+var customer_service_1 = require("../services/customer.service");
+var property_service_1 = require("../services/property.service");
+var lead_service_1 = require("../services/lead.service");
 var utils_1 = require("../../../utils/utils");
 var BusinessController = /** @class */ (function () {
     function BusinessController() {
@@ -69,25 +78,170 @@ var BusinessController = /** @class */ (function () {
             return __generator(this, function (_a) {
                 switch (_a.label) {
                     case 0: return [4 /*yield*/, (0, typeorm_1.getManager)().transaction(function (transaction) { return __awaiter(_this, void 0, void 0, function () {
-                            var businessService, result, businessEntity, error_2;
+                            var businessService, result, administratorService, managerService, advisorService, brokerService, secretaryService, ownerService, customerService, propertyService, leadService, administratorEntity, managerEntity, advisorEntity, brokerEntity, secretaryEntity, ownerEntity, customerEntity, propertyEntity, leadEntity, businessEntity, error_2;
                             return __generator(this, function (_a) {
                                 switch (_a.label) {
                                     case 0:
-                                        _a.trys.push([0, 4, , 5]);
+                                        _a.trys.push([0, 31, , 32]);
                                         businessService = new business_service_1.BusinessService();
                                         result = businessService.validateData(request.body);
-                                        if (!result) return [3 /*break*/, 2];
+                                        if (!result) return [3 /*break*/, 29];
+                                        administratorService = new administrator_service_1.AdministratorService();
+                                        managerService = new manager_service_1.ManagerService();
+                                        advisorService = new advisor_service_1.AdvisorService();
+                                        brokerService = new broker_service_1.BrokerService();
+                                        secretaryService = new secretary_service_1.SecretaryService();
+                                        ownerService = new owner_service_1.OwnerService();
+                                        customerService = new customer_service_1.CustomerService();
+                                        propertyService = new property_service_1.PropertyService();
+                                        leadService = new lead_service_1.LeadService();
                                         request.body.company = request.payload.company;
-                                        return [4 /*yield*/, businessService.create(request.body, transaction)];
+                                        request.body.dateVisit = (request.body.dateVisit) ? request.body.dateVisit : null;
+                                        request.body.dateSale = (request.body.dateSale) ? request.body.dateSale : null;
+                                        if (!(request.body.administrator && request.body.administrator.id)) return [3 /*break*/, 2];
+                                        return [4 /*yield*/, administratorService.read(request.body.administrador.id)];
                                     case 1:
+                                        administratorEntity = _a.sent();
+                                        if (administratorEntity) {
+                                            request.body.administrador = administratorEntity;
+                                        }
+                                        else {
+                                            request.body.administrator = null;
+                                        }
+                                        return [3 /*break*/, 3];
+                                    case 2:
+                                        request.body.administrator = null;
+                                        _a.label = 3;
+                                    case 3:
+                                        if (!(request.body.manager && request.body.manager.id)) return [3 /*break*/, 5];
+                                        return [4 /*yield*/, managerService.read(request.body.manager.id)];
+                                    case 4:
+                                        managerEntity = _a.sent();
+                                        if (managerEntity) {
+                                            request.body.manager = managerEntity;
+                                        }
+                                        else {
+                                            request.body.manager = null;
+                                        }
+                                        return [3 /*break*/, 6];
+                                    case 5:
+                                        request.body.manager = null;
+                                        _a.label = 6;
+                                    case 6:
+                                        if (!(request.body.advisor && request.body.advisor.id)) return [3 /*break*/, 8];
+                                        return [4 /*yield*/, advisorService.read(request.body.advisor.id)];
+                                    case 7:
+                                        advisorEntity = _a.sent();
+                                        if (advisorEntity) {
+                                            request.body.advisor = advisorEntity;
+                                        }
+                                        else {
+                                            request.body.advisor = null;
+                                        }
+                                        return [3 /*break*/, 9];
+                                    case 8:
+                                        request.body.advisor = null;
+                                        _a.label = 9;
+                                    case 9:
+                                        if (!(request.body.broker && request.body.broker.id)) return [3 /*break*/, 11];
+                                        return [4 /*yield*/, brokerService.read(request.body.broker.id)];
+                                    case 10:
+                                        brokerEntity = _a.sent();
+                                        if (brokerEntity) {
+                                            request.body.broker = brokerEntity;
+                                        }
+                                        else {
+                                            request.body.broker = null;
+                                        }
+                                        return [3 /*break*/, 12];
+                                    case 11:
+                                        request.body.broker = null;
+                                        _a.label = 12;
+                                    case 12:
+                                        if (!(request.body.secretary && request.body.secretary.id)) return [3 /*break*/, 14];
+                                        return [4 /*yield*/, secretaryService.read(request.body.secretary.id)];
+                                    case 13:
+                                        secretaryEntity = _a.sent();
+                                        if (secretaryEntity) {
+                                            request.body.secretary = secretaryEntity;
+                                        }
+                                        else {
+                                            request.body.secretary = null;
+                                        }
+                                        return [3 /*break*/, 15];
+                                    case 14:
+                                        request.body.secretary = null;
+                                        _a.label = 15;
+                                    case 15:
+                                        if (!(request.body.owner && request.body.owner.id)) return [3 /*break*/, 17];
+                                        return [4 /*yield*/, ownerService.read(request.body.owner.id)];
+                                    case 16:
+                                        ownerEntity = _a.sent();
+                                        if (ownerEntity) {
+                                            request.body.owner = ownerEntity;
+                                        }
+                                        else {
+                                            request.body.owner = null;
+                                        }
+                                        return [3 /*break*/, 18];
+                                    case 17:
+                                        request.body.owner = null;
+                                        _a.label = 18;
+                                    case 18:
+                                        if (!(request.body.customer && request.body.customer.id)) return [3 /*break*/, 20];
+                                        return [4 /*yield*/, customerService.read(request.body.customer.id)];
+                                    case 19:
+                                        customerEntity = _a.sent();
+                                        if (customerEntity) {
+                                            request.body.customer = customerEntity;
+                                        }
+                                        else {
+                                            request.body.customer = null;
+                                        }
+                                        return [3 /*break*/, 21];
+                                    case 20:
+                                        request.body.customer = null;
+                                        _a.label = 21;
+                                    case 21:
+                                        if (!(request.body.property && request.body.property.id)) return [3 /*break*/, 23];
+                                        return [4 /*yield*/, propertyService.read(request.body.property.id)];
+                                    case 22:
+                                        propertyEntity = _a.sent();
+                                        if (propertyEntity) {
+                                            request.body.property = propertyEntity;
+                                        }
+                                        else {
+                                            request.body.property = null;
+                                        }
+                                        return [3 /*break*/, 24];
+                                    case 23:
+                                        request.body.property = null;
+                                        _a.label = 24;
+                                    case 24:
+                                        if (!(request.body.lead && request.body.lead.id)) return [3 /*break*/, 26];
+                                        return [4 /*yield*/, leadService.read(request.body.lead.id)];
+                                    case 25:
+                                        leadEntity = _a.sent();
+                                        if (leadEntity) {
+                                            request.body.lead = leadEntity;
+                                        }
+                                        else {
+                                            request.body.lead = null;
+                                        }
+                                        return [3 /*break*/, 27];
+                                    case 26:
+                                        request.body.lead = null;
+                                        _a.label = 27;
+                                    case 27: return [4 /*yield*/, businessService.create(request.body, transaction)];
+                                    case 28:
                                         businessEntity = _a.sent();
                                         return [2 /*return*/, response.status(201).json(businessEntity)];
-                                    case 2: return [2 /*return*/, response.status(400).json({ message: utils_1.statusMessages[400] + " " + utils_1.returnMessages[0] })];
-                                    case 3: return [3 /*break*/, 5];
-                                    case 4:
+                                    case 29: return [2 /*return*/, response.status(400).json({ message: utils_1.statusMessages[400] + " " + utils_1.returnMessages[0] })];
+                                    case 30: return [3 /*break*/, 32];
+                                    case 31:
                                         error_2 = _a.sent();
                                         return [2 /*return*/, response.status(500).json({ message: error_2.message })];
-                                    case 5: return [2 /*return*/];
+                                    case 32: return [2 /*return*/];
                                 }
                             });
                         }); })];
