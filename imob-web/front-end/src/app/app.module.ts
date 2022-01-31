415128df4dc58e9749c8d4312693e240a8890cf4
Wellington Felix <wafsolucoestecnologicas@gmail.com>
2022-01-31 20:36:16 -0300

    Corrigindo espaçamento dos arquivos.

--- imob-web/front-end/src/app/app.module.ts
+++ imob-web/front-end/src/app/app.module.ts
@@ -7,15 +7,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent
+	],
+	imports: [
+		BrowserModule,
+		AppRoutingModule,
+		BrowserAnimationsModule
+	],
+	providers: [],
+	bootstrap: [AppComponent]
 })
 export class AppModule { }
