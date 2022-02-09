c13d123ae05ea1e4ce43e28d3d3ae3ea5d847847
Wellington Felix <wafsolucoestecnologicas@gmail.com>
2022-02-08 22:11:25 -0300

    Importando e exportando módulos.

--- imob-web/front-end/src/app/shared/shared.module.ts
+++ imob-web/front-end/src/app/shared/shared.module.ts
@@ -1,15 +1,40 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatCardModule } from '@angular/material/card';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
 	declarations: [],
 	imports: [
 		CommonModule,
-		FlexLayoutModule
+		RouterModule,
+		FlexLayoutModule,
+		MatSidenavModule,
+		MatToolbarModule,
+		MatCardModule,
+		MatMenuModule,
+		MatListModule,
+		MatIconModule,
+		MatButtonModule
 	],
 	exports: [
-		FlexLayoutModule
+		RouterModule,
+		FlexLayoutModule,
+		MatSidenavModule,
+		MatToolbarModule,
+		MatCardModule,
+		MatMenuModule,
+		MatListModule,
+		MatIconModule,
+		MatButtonModule
 	]
 })
 export class SharedModule { }
