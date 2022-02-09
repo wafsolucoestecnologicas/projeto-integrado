11d5e672acaf477b2363ad29ca62c9edd4755fcb
Wellington Felix <wafsolucoestecnologicas@gmail.com>
2022-02-08 22:12:46 -0300

    Importando módulos.

--- imob-web/front-end/src/app/app.module.ts
+++ imob-web/front-end/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { CoreModule } from './core/core.module';
 import { SharedModule } from './shared/shared.module';
+import { TemplatesModule } from './components/templates/templates.module';
 
 import { AppComponent } from './app.component';
 
@@ -17,7 +18,8 @@ import { AppComponent } from './app.component';
 		BrowserAnimationsModule,
 		AppRoutingModule,
 		CoreModule,
-		SharedModule
+		SharedModule,
+		TemplatesModule
 	],
 	providers: [],
 	bootstrap: [AppComponent]
