6491b9053ecdf2bf7adaa641e87f8d01e72773c4
Wellington Felix <wafsolucoestecnologicas@gmail.com>
2022-02-13 21:22:40 -0300

    Criando componente de resete de senha.

---
+++ imob-web/front-end/src/app/components/authentication/reset/reset.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Subscription } from 'rxjs';
+
+@Component({
+    selector: 'imob-reset',
+    templateUrl: './reset.component.html',
+    styleUrls: ['./reset.component.css']
+})
+export class ResetComponent implements OnInit, OnDestroy {
+
+    private subscriptions: Subscription[];
+    public formGroup: FormGroup;
+
+    constructor(
+		private readonly _formBuilder: FormBuilder
+	) {
+        this.subscriptions = new Array<Subscription>();
+    }
+
+    public ngOnInit(): void {
+        this.formGroup = this._formBuilder.group({
+            email: [null, [Validators.required, Validators.email]]
+        });
+    }
+
+    public ngOnDestroy(): void {
+        this.subscriptions.forEach((subscription: Subscription) => subscription.unsubscribe());
+    }
+
+    public onSubmit(): void {
+        if (this.formGroup.valid) {
+            console.log(this.formGroup.value);
+        }
+    }
+
+}
