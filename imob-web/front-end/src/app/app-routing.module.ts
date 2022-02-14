33f643197561594e1e70b3b11051cf3ff196dbc3
Wellington Felix <wafsolucoestecnologicas@gmail.com>
2022-02-13 21:25:55 -0300

    Adicionando rotas de login e conteúdo principal.

--- imob-web/front-end/src/app/app-routing.module.ts
+++ imob-web/front-end/src/app/app-routing.module.ts
@@ -1,10 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+    {
+        path: '',
+        loadChildren: () =>
+            import('./components/authentication/authentication.module').then(
+                (module) => module.AuthenticationModule
+            )
+    },
+    {
+        path: 'content',
+        loadChildren: () =>
+            import('./components/templates/templates.module').then(
+				(module) => module.TemplatesModule
+			)
+    }
+];
 
 @NgModule({
-	imports: [RouterModule.forRoot(routes)],
-	exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
