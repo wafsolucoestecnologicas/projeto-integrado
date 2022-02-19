497217b27b984e9096c4ae0554aeba6eec7294d9
Wellington Felix <wafsolucoestecnologicas@gmail.com>
2022-02-19 11:42:20 -0300

    Criando método logout, ocultando logo e diminuindo header.

--- imob-web/front-end/src/app/components/templates/header/header.component.ts
+++ imob-web/front-end/src/app/components/templates/header/header.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { AuthenticationService } from 'src/app/core/services/authentication.service';
 
 @Component({
     selector: 'imob-header',
@@ -7,8 +10,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-    constructor() {}
+    constructor(
+        private readonly _router: Router,
+        private readonly _authenticationService: AuthenticationService
+    ) {}
 
     public ngOnInit(): void {}
+
+    public logout(): void {
+        this._authenticationService.logout();
+        this._router.navigate(['login']);
+    }
 	
 }
