cb8ae0a179ae61f0941ba5b47b8ef5304b7baffa
Wellington Felix <wafsolucoestecnologicas@gmail.com>
2022-02-22 21:28:22 -0300

    Importando módulo de dialogs e interceptor para criação do cabeçalho nas requisições.

--- imob-web/front-end/src/app/app.module.ts
+++ imob-web/front-end/src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { CoreModule } from './core/core.module';
 import { SharedModule } from './shared/shared.module';
+import { DialogsModule } from './components/dialogs/dialogs.module';
 
 import { AppComponent } from './app.component';
 
 import { AuthenticationService } from './core/services/authentication.service';
 import { UserService } from './core/services/user.service';
+import { CompanyService } from './core/services/company.service';
 import { AlertService } from './shared/services/alert.service';
+
+import { HeaderInterceptor } from './shared/interceptors/header.interceptor';
 
 @NgModule({
 	declarations: [
@@ -21,12 +26,19 @@ import { AlertService } from './shared/services/alert.service';
 		BrowserAnimationsModule,
 		AppRoutingModule,
 		CoreModule,
-		SharedModule
+		SharedModule,
+		DialogsModule
 	],
 	providers: [
 		AuthenticationService,
 		UserService,
-		AlertService
+		CompanyService,
+		AlertService,
+		{
+			provide: HTTP_INTERCEPTORS,
+			useClass: HeaderInterceptor,
+			multi: true
+		}
 	],
 	bootstrap: [AppComponent]
 })
