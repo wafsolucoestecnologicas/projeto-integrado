3681798d796ba95890f85d541f779accce61bd9d
Wellington Felix <wafsolucoestecnologicas@gmail.com>
2022-02-22 21:25:22 -0300

    Criando método para abrir o dialog de imobiliária.

--- imob-web/front-end/src/app/components/templates/header/header.component.ts
+++ imob-web/front-end/src/app/components/templates/header/header.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { MatDialog } from '@angular/material/dialog';
 
 import { AuthenticationService } from 'src/app/core/services/authentication.service';
+import { CompanyComponent } from '../../dialogs/company/company.component';
 
 @Component({
     selector: 'imob-header',
@@ -12,10 +14,17 @@ export class HeaderComponent implements OnInit {
 
     constructor(
         private readonly _router: Router,
-        private readonly _authenticationService: AuthenticationService
+        private readonly _authenticationService: AuthenticationService,
+        public _matDialog: MatDialog
     ) {}
 
     public ngOnInit(): void {}
+
+    public openDialog(): void {
+        this._matDialog.open(CompanyComponent, {
+            width: '700px'
+        });
+    }
 
     public logout(): void {
         this._authenticationService.logout();
