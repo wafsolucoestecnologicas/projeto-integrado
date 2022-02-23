0ab826113f0c09c8cd746622e0127c0dc8e1cdd1
Wellington Felix <wafsolucoestecnologicas@gmail.com>
2022-02-22 21:25:51 -0300

    Importando módulo ngx mask.

--- imob-web/front-end/src/app/core/core.module.ts
+++ imob-web/front-end/src/app/core/core.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgxMaskModule } from 'ngx-mask';
 
 @NgModule({
 	declarations: [],
 	imports: [
-		CommonModule
+		CommonModule,
+		NgxMaskModule.forRoot()
 	]
 })
 export class CoreModule { }
