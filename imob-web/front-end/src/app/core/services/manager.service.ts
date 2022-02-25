299cfca190c98048162f63378031c7ac9314ca3e
Wellington Felix <wafsolucoestecnologicas@gmail.com>
2022-02-25 12:02:07 -0300

    Criando serviço de gestores.

---
+++ imob-web/front-end/src/app/core/services/manager.service.ts
@@ -0,0 +1,73 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { take, map, catchError } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+
+import { Manager, UpdateManager, DeleteManager } from '../interfaces/manager.interface';
+import { ManagerRoutes } from '../enums/manager.enum';
+import { AlertService } from 'src/app/shared/services/alert.service';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class ManagerService {
+
+    private ROUTES: typeof ManagerRoutes;
+
+    constructor(
+		private readonly http: HttpClient,
+		private readonly _alertService: AlertService
+	) {
+        this.ROUTES = ManagerRoutes;
+    }
+
+    public index(): Observable<Manager[]> {
+        return this.http.get<Manager[]>(`${environment.URL}/${this.ROUTES.MANAGERS}`).pipe(
+            take(1),
+            map((response: Manager[]) => response),
+            catchError((error: HttpErrorResponse) =>
+                this._alertService.openSnackBar(
+                    `Ocorreu um erro ao listar os gestores! - ${error.statusText}`
+                )
+            )
+        );
+    }
+
+    public read(id: number): Observable<Manager> {
+        return this.http.get<Manager>(`${environment.URL}/${this.ROUTES.MANAGERS}/${id}`).pipe(
+			take(1),
+            map((response: Manager) => response),
+            catchError((error: HttpErrorResponse) =>
+                this._alertService.openSnackBar(
+                    `Ocorreu um erro ao listar o gestor! - ${error.statusText}`
+                )
+            )
+		);
+    }
+
+    public update(body: UpdateManager, id: number): Observable<Manager> {
+        return this.http.put<Manager>(`${environment.URL}/${this.ROUTES.MANAGERS}/${id}`, body).pipe(
+			take(1),
+            map((response: Manager) => response),
+            catchError((error: HttpErrorResponse) =>
+                this._alertService.openSnackBar(
+                    `Ocorreu um erro ao atualizar o gestor! - ${error.statusText}`
+                )
+            )
+		);
+    }
+
+    public delete(id: number): Observable<DeleteManager> {
+        return this.http.delete<DeleteManager>(`${environment.URL}/${this.ROUTES.MANAGERS}/${id}`).pipe(
+			take(1),
+            map((response: DeleteManager) => response),
+            catchError((error: HttpErrorResponse) =>
+                this._alertService.openSnackBar(
+                    `Ocorreu um erro ao deleltar o gestor! - ${error.statusText}`
+                )
+            )
+		);
+    }
+	
+}
