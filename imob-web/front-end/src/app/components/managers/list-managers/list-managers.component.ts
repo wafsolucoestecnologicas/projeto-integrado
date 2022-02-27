91d17f75efa994a88033d5c8fce4f5ba5e044cbd
Wellington Felix <wellington.felix@fornecedores.sicoob.com.br>
2022-02-27 17:58:21 -0300

    Refatorando listagem de gestores.

--- imob-web/front-end/src/app/components/managers/list-managers/list-managers.component.ts
+++ imob-web/front-end/src/app/components/managers/list-managers/list-managers.component.ts
@@ -1,10 +1,13 @@
-import { Component, OnInit, AfterViewInit, OnDestroy, ViewChild } from '@angular/core';
+import { Component, OnInit, OnDestroy, ViewChild, AfterViewInit } from '@angular/core';
+import { ActivatedRoute, Data } from '@angular/router';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
 import { Subscription } from 'rxjs';
 
+import { AlertService } from 'src/app/shared/services/alert.service';
 import { Manager } from 'src/app/core/interfaces/manager.interface';
 import { ManagerService } from 'src/app/core/services/manager.service';
+import { Masks } from 'src/app/shared/enums/masks.enum';
 
 @Component({
     selector: 'imob-list-managers',
@@ -19,41 +22,51 @@ export class ListManagersComponent implements OnInit, AfterViewInit, OnDestroy {
     private subscriptions: Subscription[];
     private managers: Manager[];
     public dataSource: MatTableDataSource<Manager>;
-	public displayedColumns: string[];
+    public displayedColumns: string[];
     public show: boolean;
+    public MASKS: typeof Masks;
 
-    constructor(private readonly _managerService: ManagerService) {
+    constructor(
+        private readonly _activatedRoute: ActivatedRoute,
+        private readonly _alertService: AlertService,
+        private readonly _managerService: ManagerService
+    ) {
         this.subscriptions = new Array<Subscription>();
         this.managers = new Array<Manager>();
-		this.displayedColumns = new Array<string>();
+        this.displayedColumns = new Array<string>(
+            'name',
+            'surname',
+            'CPF',
+            'email',
+            'cellPhone',
+            'options'
+        );
         this.show = false;
+        this.MASKS = Masks;
     }
 
     public ngOnInit(): void {
-		this.displayedColumns = ['id', 'name', 'surname', 'CPF', 'email'];
-		this.indexManagers();
-	}
+        const subscription: Subscription = this._activatedRoute
+            .data
+            .subscribe((data: Data) => {
+                if (data && data.managers && data.managers.length > 0) {
+                    this.managers = data['managers'];
+                    this.show = true;
+                    this.dataSource = new MatTableDataSource<Manager>(this.managers);
+                } else {
+                    this._alertService.openSnackBar('Não existem gestores cadastrados na base de dados!');
+                }
+            });
 
-    public ngAfterViewInit(): void {debugger
-        this.dataSource = new MatTableDataSource<Manager>(this.managers);
+        this.subscriptions.push(subscription);
+    }
+
+    public ngAfterViewInit(): void {
         this.dataSource.paginator = this.paginator;
     }
 
     public ngOnDestroy(): void {
         this.subscriptions.forEach((subscription: Subscription) => subscription.unsubscribe());
-    }
-
-    private indexManagers(): void {
-        const subscription: Subscription = this._managerService
-			.index()
-			.subscribe((data: Manager[]) => {
-				if (data) {
-					this.managers = data;
-					this.show = true;
-				}
-        });
-
-		this.subscriptions.push(subscription);
     }
 
 }
