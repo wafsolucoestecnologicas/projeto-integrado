a51a19af8bc517c0b9c07985442e4bdb5eac7e44
Wellington Felix <wellington.felix@fornecedores.sicoob.com.br>
2022-02-28 16:40:17 -0300

    Criando resolver para carregamento de corretores.

---
+++ imob-web/front-end/src/app/components/brokers/shared/resolvers/broker.resolver.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { Resolve, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { Observable } from 'rxjs';
+
+import { Broker } from 'src/app/core/interfaces/broker.interface';
+import { BrokerService } from 'src/app/core/services/broker.service';
+
+@Injectable()
+export class BrokerResolver implements Resolve<Broker[]> {
+
+	constructor(
+		private readonly _brokerService: BrokerService
+	) {}
+
+    public resolve(
+		route: ActivatedRouteSnapshot,
+		state: RouterStateSnapshot
+	): Observable<Broker[]> {
+        return this._brokerService.index();
+    }
+	
+}
