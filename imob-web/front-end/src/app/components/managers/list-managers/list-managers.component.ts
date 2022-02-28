9b131cadfb4d546cc1d9266fc5e5c661b9ec5219
Wellington Felix <wellington.felix@fornecedores.sicoob.com.br>
2022-02-28 16:44:55 -0300

    Refatorando componente.

--- imob-web/front-end/src/app/components/managers/list-managers/list-managers.component.ts
+++ imob-web/front-end/src/app/components/managers/list-managers/list-managers.component.ts
@@ -1,12 +1,11 @@
-import { Component, OnInit, OnDestroy, ViewChild, AfterViewInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit, OnDestroy, ViewChild } from '@angular/core';
 import { ActivatedRoute, Data } from '@angular/router';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
 import { Subscription } from 'rxjs';
 
-import { AlertService } from 'src/app/shared/services/alert.service';
 import { Manager } from 'src/app/core/interfaces/manager.interface';
-import { ManagerService } from 'src/app/core/services/manager.service';
+import { AlertService } from 'src/app/shared/services/alert.service';
 import { Masks } from 'src/app/shared/enums/masks.enum';
 
 @Component({
@@ -23,13 +22,11 @@ export class ListManagersComponent implements OnInit, AfterViewInit, OnDestroy {
     private managers: Manager[];
     public dataSource: MatTableDataSource<Manager>;
     public displayedColumns: string[];
-    public show: boolean;
     public MASKS: typeof Masks;
 
     constructor(
         private readonly _activatedRoute: ActivatedRoute,
-        private readonly _alertService: AlertService,
-        private readonly _managerService: ManagerService
+        private readonly _alertService: AlertService
     ) {
         this.subscriptions = new Array<Subscription>();
         this.managers = new Array<Manager>();
@@ -41,7 +38,6 @@ export class ListManagersComponent implements OnInit, AfterViewInit, OnDestroy {
             'cellPhone',
             'options'
         );
-        this.show = false;
         this.MASKS = Masks;
     }
 
@@ -51,7 +47,6 @@ export class ListManagersComponent implements OnInit, AfterViewInit, OnDestroy {
             .subscribe((data: Data) => {
                 if (data && data.managers && data.managers.length > 0) {
                     this.managers = data['managers'];
-                    this.show = true;
                     this.dataSource = new MatTableDataSource<Manager>(this.managers);
                 } else {
                     this._alertService.openSnackBar('Não existem gestores cadastrados na base de dados!');
