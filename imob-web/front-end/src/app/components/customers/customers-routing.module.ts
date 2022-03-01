69bc04a0192bd3762f0921900f022c0af6f82b4f
Wellington Felix <wellington.felix@fornecedores.sicoob.com.br>
2022-02-28 23:09:31 -0300

    Criando módulo de rotas de cliente.

---
+++ imob-web/front-end/src/app/components/customers/customers-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { ListCustomersComponent } from './list-customers/list-customers.component';
+import { CustomersResolver } from './shared/resolvers/customers.resolver';
+
+const routes: Routes = [
+    {
+        path: '',
+        component: ListCustomersComponent,
+        resolve: {
+            customers: CustomersResolver
+        }
+    }
+];
+
+@NgModule({
+    imports: [RouterModule.forChild(routes)],
+    exports: [RouterModule]
+})
+export class CustomersRoutingModule {}
