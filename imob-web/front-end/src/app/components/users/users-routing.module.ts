09eaad688fe2df5bddc65f4a995265c8544332a9
Wellington Felix <wellington.felix@fornecedores.sicoob.com.br>
2022-03-10 09:25:23 -0300

    Importando componentes e adicionando rotas.

--- imob-web/front-end/src/app/components/users/users-routing.module.ts
+++ imob-web/front-end/src/app/components/users/users-routing.module.ts
@@ -2,16 +2,39 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { ListUsersComponent } from './list-users/list-users.component';
+import { CreateUserComponent } from './create-user/create-user.component';
+import { EditUserComponent } from './edit-user/edit-user.component';
+
 import { UsersResolver } from './shared/resolvers/users.resolver';
+import { UserResolver } from './shared/resolvers/user.resolver';
+import { ViewUserComponent } from './view-user/view-user.component';
 
 const routes: Routes = [
-	{
-		path: '',
-		component: ListUsersComponent,
-		resolve: {
-			users: UsersResolver
-		}
-	}
+    {
+        path: 'create',
+        component: CreateUserComponent
+    },
+    {
+        path: 'list',
+        component: ListUsersComponent,
+        resolve: {
+            users: UsersResolver
+        }
+    },
+    {
+        path: 'view/:id',
+        component: ViewUserComponent,
+        resolve: {
+            user: UserResolver
+        }
+    },
+    {
+        path: 'edit/:id',
+        component: EditUserComponent,
+        resolve: {
+            user: UserResolver
+        }
+    }
 ];
 
 @NgModule({
--- imob-web/front-end/src/app/components/users/users.module.ts
+++ imob-web/front-end/src/app/components/users/users.module.ts
@@ -4,13 +4,23 @@ import { CommonModule } from '@angular/common';
 import { UsersRoutingModule } from './users-routing.module';
 import { SharedModule } from 'src/app/shared/shared.module';
 
+import { CreateUserComponent } from './create-user/create-user.component';
 import { ListUsersComponent } from './list-users/list-users.component';
+import { ViewUserComponent } from './view-user/view-user.component';
+import { EditUserComponent } from './edit-user/edit-user.component';
+import { DeleteUserComponent } from './delete-user/delete-user.component';
 import { FormatUserTypePipe } from './shared/pipes/format-user-type.pipe';
+
 import { UsersResolver } from './shared/resolvers/users.resolver';
+import { UserResolver } from './shared/resolvers/user.resolver';
 
 @NgModule({
     declarations: [
+		CreateUserComponent,
 		ListUsersComponent,
+		ViewUserComponent,
+		EditUserComponent,
+		DeleteUserComponent,
 		FormatUserTypePipe
 	],
     imports: [
@@ -19,7 +29,8 @@ import { UsersResolver } from './shared/resolvers/users.resolver';
 		SharedModule
 	],
     providers: [
-		UsersResolver
+		UsersResolver,
+		UserResolver
 	]
 })
 export class UsersModule {}
