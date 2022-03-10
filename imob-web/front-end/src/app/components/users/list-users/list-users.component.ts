26154a36321c814b036c7c469c8f7dfc65ecdf08
Wellington Felix <wellington.felix@fornecedores.sicoob.com.br>
2022-03-10 09:19:37 -0300

    Refatorando componente de listagem de usuário.

--- imob-web/front-end/src/app/components/users/list-users/list-users.component.ts
+++ imob-web/front-end/src/app/components/users/list-users/list-users.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit, AfterViewInit, OnDestroy, ViewChild } from '@angular/core';
-import { MatPaginator } from '@angular/material/paginator';
+import { Router, ActivatedRoute, Data } from '@angular/router';
 import { MatTableDataSource } from '@angular/material/table';
-import { ActivatedRoute, Data } from '@angular/router';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatDialog, MatDialogRef } from '@angular/material/dialog';
 import { Subscription } from 'rxjs';
 
+import { DeleteUserComponent } from '../delete-user/delete-user.component';
 import { User } from 'src/app/core/interfaces/user.interface';
 import { AlertService } from 'src/app/shared/services/alert.service';
 
@@ -18,14 +20,17 @@ export class ListUsersComponent implements OnInit, AfterViewInit, OnDestroy {
     public paginator: MatPaginator;
 
     private subscriptions: Subscription[];
+    private user: User | undefined;
     private users: User[];
     public dataSource: MatTableDataSource<User>;
     public displayedColumns: string[];
-    public show: boolean;
+    public path: string;
 
     constructor(
+        private readonly _router: Router,
         private readonly _activatedRoutes: ActivatedRoute,
-        private readonly _alertService: AlertService
+        private readonly _alertService: AlertService,
+        public _matDialog: MatDialog
     ) {
         this.subscriptions = new Array<Subscription>();
         this.users = new Array<User>();
@@ -37,23 +42,22 @@ export class ListUsersComponent implements OnInit, AfterViewInit, OnDestroy {
             'isAdvisor',
             'isBroker',
             'isSecretary',
-			'options'
+            'options'
         );
-        this.show = false;
+        this.path = '/content/users';
     }
 
     public ngOnInit(): void {
         const subscription: Subscription = this._activatedRoutes
-			.data
-			.subscribe((data: Data) => {
-				if (data && data['users'] && data['users'].length > 0) {
-					this.users = data['users'];
-					this.show = true;
-					this.dataSource = new MatTableDataSource<User>(this.users);
-				} else {
-					this._alertService.openSnackBar('Não existem usuários cadastrados na base de dados!');
-				}
-			});
+            .data
+            .subscribe((data: Data) => {
+                if (data && data['users'] && data['users'].length > 0) {
+                    this.users = data['users'];
+                    this.dataSource = new MatTableDataSource<User>(this.users);
+                } else {
+                    this._alertService.openSnackBar('Não existem usuários cadastrados na base de dados!');
+                }
+            });
 
         this.subscriptions.push(subscription);
     }
@@ -64,6 +68,38 @@ export class ListUsersComponent implements OnInit, AfterViewInit, OnDestroy {
 
     public ngOnDestroy(): void {
         this.subscriptions.forEach((subscription: Subscription) => subscription.unsubscribe());
+    }
+
+    public redirectToEdit(id: number): void {
+        this._router.navigate([`${this.path}/edit`, id]);
+    }
+
+    public redirectToView(id: number): void {
+        this._router.navigate([`${this.path}/view`, id]);
+    }
+
+    public openDialog(id: number): void {
+        const dialogRef: MatDialogRef<DeleteUserComponent> = this._matDialog
+            .open(DeleteUserComponent, {
+                width: '600px',
+                data: {
+                    user: this.users.find((user: User) => user.id === id)
+                }
+            });
+
+        const subscription: Subscription = dialogRef
+            .afterClosed()
+            .subscribe((id: number) => {
+                this.user = this.users.find((user: User) => user.id === id);
+
+                if (this.user) {
+                    this.users = this.users.filter((user: User) => user.id !== id);
+                    this.dataSource.data.splice(this.dataSource.data.indexOf(this.user, 1));
+                    this.dataSource._updateChangeSubscription();
+                }
+            });
+
+        this.subscriptions.push(subscription);
     }
 
 }
