1642053bc3324002666c35093dee245390904474
Wellington Felix <wellington.felix@fornecedores.sicoob.com.br>
2022-03-10 18:35:26 -0300

    Removendo import de módulo.

--- imob-web/front-end/src/app/app.module.ts
+++ imob-web/front-end/src/app/app.module.ts
@@ -6,7 +6,6 @@ import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { CoreModule } from './core/core.module';
 import { SharedModule } from './shared/shared.module';
-import { DialogsModule } from './components/dialogs/dialogs.module';
 
 import { AppComponent } from './app.component';
 
@@ -35,8 +34,7 @@ import { AlertService } from './shared/services/alert.service';
 		BrowserAnimationsModule,
 		AppRoutingModule,
 		CoreModule,
-		SharedModule,
-		DialogsModule
+		SharedModule
 	],
 	providers: [
 		{
