89a85727ae30cc9d7b1546dacd0d0f65bab595d8
Wellington Felix <wellington.felix@fornecedores.sicoob.com.br>
2022-03-10 09:13:44 -0300

    Criando serviço de perfil de usuário.

---
+++ imob-web/front-end/src/app/core/services/profile.service.ts
@@ -0,0 +1,45 @@
+import { Injectable } from '@angular/core';
+
+import { UserType } from '../interfaces/profile.interface';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class ProfileService {
+
+	private _usersTypes: UserType[];
+
+    constructor() {
+		this._usersTypes = new Array<UserType>(
+			{
+				disabled: true,
+				description: 'Administrador',
+				value: 1
+			},
+			{
+				disabled: false,
+				description: 'Gestor',
+				value: 2
+			},
+			{
+				disabled: false,
+				description: 'Despachante',
+				value: 3
+			},
+			{
+				disabled: false,
+				description: 'Corretor',
+				value: 4
+			},
+			{
+				disabled: false,
+				description: 'Secretária',
+				value: 5
+			}
+		);
+	}
+
+	public get usersTypes(): UserType[] {
+		return this._usersTypes;
+	}
+}
