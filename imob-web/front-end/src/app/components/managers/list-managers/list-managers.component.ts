f5b0577843ac814ba4e3fac93c1c3f9a49b2c475
Wellington Felix <wellington.felix@fornecedores.sicoob.com.br>
2022-03-12 22:20:38 -0300

    Refatorando listagem de gestores.

--- imob-web/front-end/src/app/components/managers/list-managers/list-managers.component.ts
+++ imob-web/front-end/src/app/components/managers/list-managers/list-managers.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, AfterViewInit, OnDestroy, ViewChild } from '@angular/core';
-import { ActivatedRoute, Data } from '@angular/router';
+import { Router, ActivatedRoute, Data } from '@angular/router';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
 import { Subscription } from 'rxjs';
@@ -22,9 +22,11 @@ export class ListManagersComponent implements OnInit, AfterViewInit, OnDestroy {
     private managers: Manager[];
     public dataSource: MatTableDataSource<Manager>;
     public displayedColumns: string[];
+    public path: string;
     public MASKS: typeof Masks;
 
     constructor(
+        private readonly _router: Router,
         private readonly _activatedRoute: ActivatedRoute,
         private readonly _alertService: AlertService
     ) {
@@ -38,6 +40,7 @@ export class ListManagersComponent implements OnInit, AfterViewInit, OnDestroy {
             'cellPhone',
             'options'
         );
+        this.path = '/content/managers';
         this.MASKS = Masks;
     }
 
@@ -63,5 +66,13 @@ export class ListManagersComponent implements OnInit, AfterViewInit, OnDestroy {
     public ngOnDestroy(): void {
         this.subscriptions.forEach((subscription: Subscription) => subscription.unsubscribe());
     }
+
+    public redirectToView(id: number): void {
+		this._router.navigate([`${this.path}/view`, id]);
+	}
+
+	public redirectToEdit(id: number): void {
+		this._router.navigate([`${this.path}/edit`, id]);
+	}
 
 }
