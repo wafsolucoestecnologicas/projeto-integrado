9d84724686b99748b69a82a598fe2275f6f15fc0
Wellington Felix <wellington.felix@fornecedores.sicoob.com.br>
2022-03-15 21:56:35 -0300

    Criando componente de visualização de cliente.

---
+++ imob-web/front-end/src/app/components/customers/view-customer/view-customer.component.ts
@@ -0,0 +1,50 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Router, ActivatedRoute, Data } from '@angular/router';
+import { Subscription } from 'rxjs';
+
+import { Customer } from 'src/app/core/interfaces/customer.interface';
+import { AlertService } from 'src/app/shared/services/alert.service';
+import { Masks } from 'src/app/shared/enums/masks.enum';
+
+@Component({
+    selector: 'imob-view-customer',
+    templateUrl: './view-customer.component.html',
+    styleUrls: ['./view-customer.component.css']
+})
+export class ViewCustomerComponent implements OnInit, OnDestroy {
+
+    private subscriptions: Subscription[];
+    public customer: Customer;
+    public path: string;
+    public MASKS: typeof Masks;
+
+    constructor(
+        private readonly _router: Router,
+        private readonly _activatedRoute: ActivatedRoute,
+        private readonly _alertService: AlertService
+    ) {
+        this.subscriptions = new Array<Subscription>();
+        this.path = '/content/customers';
+        this.MASKS = Masks;
+    }
+
+    public ngOnInit(): void {
+        const subscription: Subscription = this._activatedRoute
+			.data
+			.subscribe((data: Data) => {
+				if (data && data['customer']) {
+					this.customer = data['customer'];
+				} else {
+					this._router.navigate([`${this.path}/list`]);
+					this._alertService.openSnackBar('Cliente não encontrado na base de dados!');
+				}
+			});
+
+        this.subscriptions.push(subscription);
+    }
+
+    public ngOnDestroy(): void {
+        this.subscriptions.forEach((subscription: Subscription) => subscription.unsubscribe());
+    }
+
+}
