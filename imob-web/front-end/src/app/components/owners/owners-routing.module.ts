798d2f3bcb04c83947571e050742594d76c534d0
Wellington Felix <wellington.felix@fornecedores.sicoob.com.br>
2022-03-15 22:06:52 -0300

    Importando componentes e criando rotas para proprietários.

--- imob-web/front-end/src/app/components/owners/owners-routing.module.ts
+++ imob-web/front-end/src/app/components/owners/owners-routing.module.ts
@@ -1,15 +1,38 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { CreateOwnerComponent } from './create-owner/create-owner.component';
 import { ListOwnersComponent } from './list-owners/list-owners.component';
+import { ViewOwnerComponent } from './view-owner/view-owner.component';
+import { EditOwnerComponent } from './edit-owner/edit-owner.component';
+
+import { OwnerResolver } from './shared/resolvers/owner.resolver';
 import { OwnersResolver } from './shared/resolvers/owners.resolver';
 
 const routes: Routes = [
+    {
+        path: 'create',
+        component: CreateOwnerComponent
+    },
     {
         path: 'list',
         component: ListOwnersComponent,
         resolve: {
             owners: OwnersResolver
+        }
+    },
+    {
+        path: 'view/:id',
+        component: ViewOwnerComponent,
+        resolve: {
+            owner: OwnerResolver
+        }
+    },
+    {
+        path: 'edit/:id',
+        component: EditOwnerComponent,
+        resolve: {
+            owner: OwnerResolver
         }
     }
 ];
--- imob-web/front-end/src/app/components/owners/owners.module.ts
+++ imob-web/front-end/src/app/components/owners/owners.module.ts
@@ -4,13 +4,21 @@ import { CommonModule } from '@angular/common';
 import { OwnersRoutingModule } from './owners-routing.module';
 import { SharedModule } from 'src/app/shared/shared.module';
 
+import { CreateOwnerComponent } from './create-owner/create-owner.component';
 import { ListOwnersComponent } from './list-owners/list-owners.component';
+import { ViewOwnerComponent } from './view-owner/view-owner.component';
+import { EditOwnerComponent } from './edit-owner/edit-owner.component';
 import { FormatCheckedPipe } from './shared/pipes/format-checked.pipe';
+
 import { OwnersResolver } from './shared/resolvers/owners.resolver';
+import { OwnerResolver } from './shared/resolvers/owner.resolver';
 
 @NgModule({
     declarations: [
+    	CreateOwnerComponent,
 		ListOwnersComponent,
+		ViewOwnerComponent,
+    	EditOwnerComponent,
   		FormatCheckedPipe
 	],
     imports: [
@@ -19,7 +27,8 @@ import { OwnersResolver } from './shared/resolvers/owners.resolver';
 		SharedModule
 	],
 	providers: [
-		OwnersResolver
+		OwnersResolver,
+		OwnerResolver
 	]
 })
 export class OwnersModule {}
