3e9843a0fedb3cd285a1d04a62237a15f57c3522
Wellington Felix <wellington.felix@fornecedores.sicoob.com.br>
2022-03-15 22:08:13 -0300

    Importando componentes e criando rotas para clientes.

--- imob-web/front-end/src/app/components/customers/customers-routing.module.ts
+++ imob-web/front-end/src/app/components/customers/customers-routing.module.ts
@@ -1,15 +1,38 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { CreateCustomerComponent } from './create-customer/create-customer.component';
 import { ListCustomersComponent } from './list-customers/list-customers.component';
+import { ViewCustomerComponent } from './view-customer/view-customer.component';
+import { EditCustomerComponent } from './edit-customer/edit-customer.component';
+
 import { CustomersResolver } from './shared/resolvers/customers.resolver';
+import { CustomerResolver } from './shared/resolvers/customer.resolver';
 
 const routes: Routes = [
+    {
+        path: 'create',
+        component: CreateCustomerComponent
+    },
     {
         path: 'list',
         component: ListCustomersComponent,
         resolve: {
             customers: CustomersResolver
+        }
+    },
+    {
+        path: 'view/:id',
+        component: ViewCustomerComponent,
+        resolve: {
+            customer: CustomerResolver
+        }
+    },
+    {
+        path: 'edit/:id',
+        component: EditCustomerComponent,
+        resolve: {
+            customer: CustomerResolver
         }
     }
 ];
--- imob-web/front-end/src/app/components/customers/customers.module.ts
+++ imob-web/front-end/src/app/components/customers/customers.module.ts
@@ -4,20 +4,29 @@ import { CommonModule } from '@angular/common';
 import { CustomersRoutingModule } from './customers-routing.module';
 import { SharedModule } from 'src/app/shared/shared.module';
 
+import { CreateCustomerComponent } from './create-customer/create-customer.component';
 import { ListCustomersComponent } from './list-customers/list-customers.component';
+import { ViewCustomerComponent } from './view-customer/view-customer.component';
+import { EditCustomerComponent } from './edit-customer/edit-customer.component';
+
 import { CustomersResolver } from './shared/resolvers/customers.resolver';
+import { CustomerResolver } from './shared/resolvers/customer.resolver';
 
 @NgModule({
     declarations: [
-		ListCustomersComponent
-	],
+        CreateCustomerComponent,
+        ListCustomersComponent,
+        ViewCustomerComponent,
+        EditCustomerComponent
+    ],
     imports: [
 		CommonModule,
 		CustomersRoutingModule,
 		SharedModule
 	],
-	providers: [
-		CustomersResolver
+    providers: [
+		CustomersResolver,
+		CustomerResolver
 	]
 })
 export class CustomersModule {}
