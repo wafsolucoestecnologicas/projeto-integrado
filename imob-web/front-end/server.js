2b3ad3911d3fe092b3440cbf26220a006f46f21d
Wellington Felix <wellington.felix@fornecedores.sicoob.com.br>
2022-03-28 20:34:31 -0300

    Refatorando código.

--- imob-web/front-end/server.js
+++ imob-web/front-end/server.js
@@ -1,9 +1,16 @@
 const express = require('express');
+const path = require('path');
 
+const name = process.env.NAME_APP || 'front-end';
+const port = process.env.PORT || 8080;
 const app = express();
 
-app.use(express.static('./dist/front-end'));
+app.use(express.static(`${__dirname}/dist/${name}`));
 
-app.get('/*', (req, res) => res.sendFile('index.html', { root: 'dist/front-end/' }));
+app.get('/*', (req, res) => {
+    res.sendFile(path.join(`${__dirname}/dist/${name}/index.html`));
+});
 
-app.listen(process.env.PORT || 8080);
+app.listen(port, () => {
+    console.log(`Servidor Rodando na Porta ${port}!`);
+});
