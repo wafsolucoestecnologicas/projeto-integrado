4d4771ff863686bb05f569227c35196111069cd9
Wellington Felix <wellington.felix@fornecedores.sicoob.com.br>
2022-04-06 12:03:13 -0300

    Chamando resolvers.

--- imob-web/front-end/src/app/components/companies/companies-routing.module.ts
+++ imob-web/front-end/src/app/components/companies/companies-routing.module.ts
@@ -7,6 +7,9 @@ import { EditCompanyComponent } from './edit-company/edit-company.component';
 
 import { CompaniesResolver } from './shared/resolvers/companies.resolver';
 import { CompanyResolver } from './shared/resolvers/company.resolver';
+import { AdressesResolver } from 'src/app/shared/resolvers/adresses.resolver';
+import { NeighborhoodsResolver } from 'src/app/shared/resolvers/neighborhoods.resolver';
+import { CitiesResolver } from 'src/app/shared/resolvers/cities.resolver';
 
 const routes: Routes = [
     {
@@ -20,14 +23,20 @@ const routes: Routes = [
         path: 'view/:id',
         component: ViewCompanyComponent,
         resolve: {
-            company: CompanyResolver
+            company: CompanyResolver,
+            adresses: AdressesResolver,
+            neighborhoods: NeighborhoodsResolver,
+            cities: CitiesResolver
         }
     },
     {
         path: 'edit/:id',
         component: EditCompanyComponent,
         resolve: {
-            company: CompanyResolver
+            company: CompanyResolver,
+            adresses: AdressesResolver,
+            neighborhoods: NeighborhoodsResolver,
+            cities: CitiesResolver
         }
     }
 ];
