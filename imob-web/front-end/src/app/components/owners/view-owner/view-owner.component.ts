906f4db5cfbde5950ef5a373d624132895cf831c
Wellington Felix <wellington.felix@fornecedores.sicoob.com.br>
2022-04-06 17:24:39 -0300

    Exibindo endereço.

--- imob-web/front-end/src/app/components/owners/view-owner/view-owner.component.ts
+++ imob-web/front-end/src/app/components/owners/view-owner/view-owner.component.ts
@@ -3,6 +3,10 @@ import { Router, ActivatedRoute, Data } from '@angular/router';
 import { Subscription } from 'rxjs';
 
 import { Owner } from 'src/app/core/interfaces/owner.interface';
+import { State } from 'src/app/core/interfaces/state.interface';
+import { City } from 'src/app/core/interfaces/city.interface';
+import { Neighborhood } from 'src/app/core/interfaces/neighborhood.interface';
+import { Address } from 'src/app/core/interfaces/address.interface';
 import { AlertService } from 'src/app/shared/services/alert.service';
 import { Masks } from 'src/app/shared/enums/masks.enum';
 
@@ -15,6 +19,10 @@ export class ViewOwnerComponent implements OnInit, OnDestroy {
 
     private subscriptions: Subscription[];
     public owner: Owner;
+    public state: State;
+    public city: City;
+    public neighborhood: Neighborhood;
+    public address: Address;
     public path: string;
     public MASKS: typeof Masks;
 
@@ -29,11 +37,21 @@ export class ViewOwnerComponent implements OnInit, OnDestroy {
     }
 
     public ngOnInit(): void {
+        this.loadOwnerData();
+    }
+
+    public ngOnDestroy(): void {
+        this.subscriptions.forEach((subscription: Subscription) => subscription.unsubscribe());
+    }
+
+    private loadOwnerData(): void {
         const subscription: Subscription = this._activatedRoute
 			.data
 			.subscribe((data: Data) => {
 				if (data && data['owner']) {
 					this.owner = data['owner'];
+
+                    this.loadAdressesData();
 				} else {
 					this._router.navigate([`${this.path}/list`]);
 					this._alertService.openSnackBar('Proprietário não encontrado na base de dados!');
@@ -43,8 +61,56 @@ export class ViewOwnerComponent implements OnInit, OnDestroy {
         this.subscriptions.push(subscription);
     }
 
-    public ngOnDestroy(): void {
-        this.subscriptions.forEach((subscription: Subscription) => subscription.unsubscribe());
+    private loadAdressesData(): void {
+        const subscription: Subscription = this._activatedRoute
+            .data
+            .subscribe((data: Data) => {
+                if (data && data['adresses'] && data['adresses'].length > 0) {
+                    this.address = data['adresses'].filter((address: Address) => this.owner.id === address.owner?.id && address.isOwner)[0];
+
+                    if (this.address) {
+                        this.loadNeighborhoodsData();
+                    }
+                }
+            });
+
+        this.subscriptions.push(subscription);
+    }
+
+    private loadNeighborhoodsData(): void {
+        const subscription: Subscription = this._activatedRoute
+            .data
+            .subscribe((data: Data) => {
+                if (data && data['neighborhoods'] && data['neighborhoods'].length > 0) {
+                    this.neighborhood = data['neighborhoods'].filter(
+                        (neighborhood: Neighborhood) => neighborhood.id === this.address.neighborhood.id
+                    )[0];
+
+                    if (this.neighborhood) {
+                        this.loadCitiesData();
+                    }
+                }
+            });
+
+        this.subscriptions.push(subscription);
+    }
+
+    private loadCitiesData(): void {
+        const subscription: Subscription = this._activatedRoute
+            .data
+            .subscribe((data: Data) => {
+                if (data && data['cities'] && data['cities'].length > 0) {
+                    this.city = data['cities'].filter(
+                        (city: City) => city.id === this.neighborhood.city.id
+                    )[0];
+
+                    if (this.city) {
+                        this.state = this.city.state;
+                    }
+                }
+            });
+
+        this.subscriptions.push(subscription);
     }
 	
 }
