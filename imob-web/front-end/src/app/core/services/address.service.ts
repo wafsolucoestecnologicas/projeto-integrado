46eed845bde46ff4168e84c38678a0ded65efe0f
Wellington Felix <wellington.felix@fornecedores.sicoob.com.br>
2022-04-06 11:53:01 -0300

    Criando serviço de endereço.

---
+++ imob-web/front-end/src/app/core/services/address.service.ts
@@ -0,0 +1,101 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { take, map, catchError } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+
+import { Address, CreateAddress, UpdateAddress, DeleteAddress, ResponseViaCEPModel } from '../interfaces/address.interface';
+import { AlertService } from 'src/app/shared/services/alert.service';
+import { AddressRoutes } from '../enums/address.enum';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class AddressService {
+
+    private ROUTES: typeof AddressRoutes;
+
+    constructor(
+		private readonly http: HttpClient,
+		private readonly _alertService: AlertService
+	) {
+        this.ROUTES = AddressRoutes;
+    }
+
+	public search(CEP: string): Observable<ResponseViaCEPModel> {
+        return this.http.get<ResponseViaCEPModel>(`${environment.URL}/${this.ROUTES.SEARCH}`, {
+			params: {
+				CEP
+			}
+		}).pipe(
+            take(1),
+            map((response: ResponseViaCEPModel) => response),
+            catchError((error: HttpErrorResponse) =>
+                this._alertService.openSnackBar(
+                    `Ocorreu um erro ao buscar o endereço do CEP! - ${error.message}`
+                )
+            )
+        );
+    }
+
+    public index(): Observable<Address[]> {
+        return this.http.get<Address[]>(`${environment.URL}/${this.ROUTES.ADRESSES}`).pipe(
+            take(1),
+            map((response: Address[]) => response),
+            catchError((error: HttpErrorResponse) =>
+                this._alertService.openSnackBar(
+                    `Ocorreu um erro ao listar os endereços! - ${error.message}`
+                )
+            )
+        );
+    }
+
+    public create(body: CreateAddress): Observable<Address> {
+        return this.http.post<Address>(`${environment.URL}/${this.ROUTES.ADRESSES}`, body).pipe(
+            take(1),
+            map((response: Address) => response),
+            catchError((error: HttpErrorResponse) =>
+                this._alertService.openSnackBar(
+                    `Ocorreu um erro ao criar um endereço! - ${error.message}`
+                )
+            )
+        );
+    }
+
+    public read(id: number): Observable<Address | undefined> {
+        return this.http.get<Address>(`${environment.URL}/${this.ROUTES.ADRESSES}/${id}`).pipe(
+            take(1),
+            map((response: Address) => response),
+            catchError((error: HttpErrorResponse) =>
+                this._alertService.openSnackBar(
+                    `Ocorreu um erro ao listar o endereço! - ${error.message}`
+                )
+            )
+        );
+    }
+
+    public update(body: UpdateAddress, id: number): Observable<Address> {
+        return this.http.put<Address>(`${environment.URL}/${this.ROUTES.ADRESSES}/${id}`, body).pipe(
+            take(1),
+            map((response: Address) => response),
+            catchError((error: HttpErrorResponse) =>
+                this._alertService.openSnackBar(
+                    `Ocorreu um erro ao atualizar o endereço! - ${error.message}`
+                )
+            )
+        );
+    }
+
+    public delete(id: number): Observable<DeleteAddress> {
+        return this.http.delete<DeleteAddress>(`${environment.URL}/${this.ROUTES.ADRESSES}/${id}`).pipe(
+            take(1),
+            map((response: DeleteAddress) => response),
+            catchError((error: HttpErrorResponse) =>
+                this._alertService.openSnackBar(
+                    `Ocorreu um erro ao deletar o endereço! - ${error.message}`
+                )
+            )
+        );
+    }
+
+}
