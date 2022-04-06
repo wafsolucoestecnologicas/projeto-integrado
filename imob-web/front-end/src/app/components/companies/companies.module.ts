135aa2992977813a4030d48736d545c7c0eb16f7
Wellington Felix <wellington.felix@fornecedores.sicoob.com.br>
2022-04-06 12:02:50 -0300

    Importando resolvers.

--- imob-web/front-end/src/app/components/companies/companies.module.ts
+++ imob-web/front-end/src/app/components/companies/companies.module.ts
@@ -10,6 +10,9 @@ import { EditCompanyComponent } from './edit-company/edit-company.component';
 
 import { CompaniesResolver } from './shared/resolvers/companies.resolver';
 import { CompanyResolver } from './shared/resolvers/company.resolver';
+import { AdressesResolver } from 'src/app/shared/resolvers/adresses.resolver';
+import { NeighborhoodsResolver } from 'src/app/shared/resolvers/neighborhoods.resolver';
+import { CitiesResolver } from 'src/app/shared/resolvers/cities.resolver';
 
 @NgModule({
     declarations: [
@@ -24,7 +27,10 @@ import { CompanyResolver } from './shared/resolvers/company.resolver';
 	],
 	providers: [
 		CompaniesResolver,
-		CompanyResolver
+		CompanyResolver,
+		AdressesResolver,
+		NeighborhoodsResolver,
+		CitiesResolver
 	]
 })
 export class CompaniesModule {}
