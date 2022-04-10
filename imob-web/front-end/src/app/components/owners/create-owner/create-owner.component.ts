c6afefd22113994c2c013b2c61ce983da0b5a1fb
Wellington Felix <wellington.felix@fornecedores.sicoob.com.br>
2022-04-09 21:01:25 -0300

    Adicionando endereço no formulário de criação de proprietário.

--- imob-web/front-end/src/app/components/owners/create-owner/create-owner.component.ts
+++ imob-web/front-end/src/app/components/owners/create-owner/create-owner.component.ts
@@ -4,9 +4,17 @@ import { Router } from '@angular/router';
 import { Subscription } from 'rxjs';
 
 import { Owner, CreateOwner } from 'src/app/core/interfaces/owner.interface';
+import { State } from 'src/app/core/interfaces/state.interface';
+import { City, CreateCity } from 'src/app/core/interfaces/city.interface';
+import { Neighborhood, CreateNeighborhood } from 'src/app/core/interfaces/neighborhood.interface';
+import { Address, CreateAddress, ResponseViaCEPModel } from 'src/app/core/interfaces/address.interface';
 import { OwnerService } from 'src/app/core/services/owner.service';
-import { Masks } from 'src/app/shared/enums/masks.enum';
+import { AddressService } from 'src/app/core/services/address.service';
+import { NeighborhoodService } from 'src/app/core/services/neighborhood.service';
+import { CityService } from 'src/app/core/services/city.service';
+import { StateService } from 'src/app/core/services/state.service';
 import { AlertService } from 'src/app/shared/services/alert.service';
+import { Masks } from 'src/app/shared/enums/masks.enum';
 
 @Component({
     selector: 'imob-create-owner',
@@ -17,6 +25,11 @@ export class CreateOwnerComponent implements OnInit, OnDestroy {
 
     private subscriptions: Subscription[];
     public formGroup: FormGroup;
+    public owner: Owner;
+    public state: State;
+    public city: City;
+    public neighborhood: Neighborhood;
+    public address: Address;
     public path: string;
     public MASKS: typeof Masks;
 
@@ -24,6 +37,10 @@ export class CreateOwnerComponent implements OnInit, OnDestroy {
         private readonly _router: Router,
         private readonly _formBuilder: FormBuilder,
         private readonly _ownerService: OwnerService,
+        private readonly _addressService: AddressService,
+        private readonly _neighborhoodService: NeighborhoodService,
+        private readonly _cityService: CityService,
+        private readonly _stateService: StateService,
         private readonly _alertService: AlertService
     ) {
         this.subscriptions = new Array<Subscription>();
@@ -41,48 +58,175 @@ export class CreateOwnerComponent implements OnInit, OnDestroy {
 
     private createFormGroup(): void {
         this.formGroup = this._formBuilder.group({
-            name: [null, [Validators.required]],
-            surname: [null, [Validators.required]],
-            email: [null, [Validators.required, Validators.email]],
-            birthDate: [null, [Validators.required]],
-            checked: [false, [Validators.required]],
-            RG: [null, [Validators.required]],
-            CPF: [null, [Validators.required]],
-            landline: [null, []],
-            cellPhone: [null, [Validators.required]],
-            profession: [null, []]
+            owner: this._formBuilder.group({
+                id: [null, []],
+                name: [null, [Validators.required]],
+                surname: [null, [Validators.required]],
+                email: [null, [Validators.required, Validators.email]],
+                birthDate: [null, [Validators.required]],
+                checked: [false, [Validators.required]],
+                RG: [null, [Validators.required]],
+                CPF: [null, [Validators.required]],
+                landline: [null, []],
+                cellPhone: [null, [Validators.required]],
+                profession: [null, []]
+            }),
+            address: this._formBuilder.group({
+                CEP: [null, [Validators.required]],
+                street: [null, [Validators.required]],
+                number: [null, [Validators.required]],
+                neighborhood: [null, [Validators.required]],
+                city: [null, [Validators.required]],
+                state: [null, [Validators.required]],
+                complement: [null, []]
+            })
         });
     }
 
-    private parseFormGroup(form: any): CreateOwner {
+    private parseOwner(form: any): CreateOwner {
         return {
-            name: form.name,
-            surname: form.surname,
-            email: form.email,
-            birthDate: form.birthDate,
-            checked: form.checked,
-            RG: form.RG,
-            CPF: form.CPF,
-            landline: form.landline,
-            cellPhone: form.cellPhone,
-            profession: form.profession
+            name: form.owner.name,
+            surname: form.owner.surname,
+            email: form.owner.email,
+            birthDate: form.owner.birthDate,
+            checked: form.owner.checked,
+            RG: form.owner.RG,
+            CPF: form.owner.CPF,
+            landline: form.owner.landline,
+            cellPhone: form.owner.cellPhone,
+            profession: form.owner.profession
         };
     }
 
-    public onSubmit(): void {
-        if (this.formGroup.valid) {
-            const subscription: Subscription = this._ownerService
-                .create(this.parseFormGroup(this.formGroup.value))
-                .subscribe((data: Owner) => {
-                    if (data) {
-                        this._router.navigate([`${this.path}/list`]);
+    private parseCity(city: string, state: State): CreateCity {
+        return {
+            city,
+            state
+        };
+    }
+
+    private parseNeighborhood(neighborhood: string, city: City): CreateNeighborhood {
+        return {
+            neighborhood,
+            city
+        };
+    }
+
+    private parseCreateAddress(form: any, neighborhood: Neighborhood): CreateAddress {
+        return {
+            street: form.address.street,
+            complement: form.address.complement,
+            number: form.address.number,
+            CEP: form.address.CEP,
+            isCompany: false,
+            isManager: false,
+            isAdvisor: false,
+            isBroker: false,
+            isSecretary: false,
+            isOwner: true,
+            isCustomer: false,
+            isProperty: false,
+            company: form.company,
+            owner: form.owner,
+            neighborhood
+        };
+    }
+
+    private createOwner(): void {
+        const subscription: Subscription = this._ownerService
+            .create(this.parseOwner(this.formGroup.value))
+            .subscribe((data: Owner) => {
+                if (data) {
+                    this.owner = data;
+                    this.formGroup.get('owner')?.patchValue(this.owner);
+                    this.findState();
+                }
+            });
+
+        this.subscriptions.push(subscription);
+    }
+
+    private createAddress(): void {
+        const subscription: Subscription = this._addressService
+            .create(this.parseCreateAddress(this.formGroup.value, this.neighborhood))
+            .subscribe((data: Address) => {
+                if (data) {
+                    this.address = data;
+                    this._router.navigate([`${this.path}/list`]);
+                    this._alertService.openSnackBar(`Proprietário ${this.owner.name} ${this.owner.surname} criado com sucesso!`);
+                }
+            });
+
+        this.subscriptions.push(subscription);
+    }
+
+    private createNeighborhood(): void {
+        const subscription: Subscription = this._neighborhoodService
+            .create(this.parseNeighborhood(this.formGroup.get('address')?.get('neighborhood')?.value, this.city))
+            .subscribe((data: Neighborhood) => {
+                if (data) {
+                    this.neighborhood = data;
+                    this.createAddress();
+                }
+            });
+
+        this.subscriptions.push(subscription);
+    }
+
+    private createCity(): void {
+        const subscription: Subscription = this._cityService
+            .create(this.parseCity(this.formGroup.get('address')?.get('city')?.value, this.state))
+            .subscribe((data: City) => {
+                if (data) {
+                    this.city = data;
+                    this.createNeighborhood();
+                }
+            });
+
+        this.subscriptions.push(subscription);
+    }
+
+    private findState(): void {
+        const subscription: Subscription = this._stateService
+            .index()
+            .subscribe((data: State[]) => {
+                if (data && data.length > 0) {
+                    this.state = data.filter(
+                        (state: State) => state.UF === this.formGroup.get('address')?.get('state')?.value
+                    )[0];
+
+                    this.createCity();
+                }
+            });
+
+        this.subscriptions.push(subscription);
+    }
+
+    public search(): void {
+        if (this.formGroup.get('address')?.get('CEP')?.value.length === 8) {
+            const subscription: Subscription = this._addressService
+                .search(this.formGroup.get('address')?.get('CEP')?.value)
+                .subscribe((data: ResponseViaCEPModel) => {
+                    if (data && data.cep) {
+                        this.formGroup.get('address')?.get('street')?.setValue(data.logradouro);
+                        this.formGroup.get('address')?.get('complement')?.setValue(data.complemento);
+                        this.formGroup.get('address')?.get('neighborhood')?.setValue(data.bairro);
+                        this.formGroup.get('address')?.get('city')?.setValue(data.localidade);
+                        this.formGroup.get('address')?.get('state')?.setValue(data.uf);
+                    } else {
                         this._alertService.openSnackBar(
-                            `Proprietário ${data.name} ${data.surname} criado com sucesso!`
+                            'API ViaCEP não encontrou nenhum endereço ou o CEP é inválido!'
                         );
                     }
                 });
 
             this.subscriptions.push(subscription);
+        }
+    }
+
+    public onSubmit(): void {
+        if (this.formGroup.valid) {
+            this.createOwner();
         }
     }
 
