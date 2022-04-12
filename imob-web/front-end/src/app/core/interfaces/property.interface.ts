d14a06ae15d7041473115714178cf6316ec58c76
Wellington Felix <wellington.felix@fornecedores.sicoob.com.br>
2022-04-12 11:04:27 -0300

    Criando interfaces para criação, atualização e deleção de imóvel.

--- imob-web/front-end/src/app/core/interfaces/property.interface.ts
+++ imob-web/front-end/src/app/core/interfaces/property.interface.ts
@@ -4,6 +4,7 @@ import { Manager } from './manager.interface';
 import { Advisor } from './advisor.interface';
 import { Broker } from './broker.interface';
 import { Secretary } from './secretary.interface';
+import { Owner } from './owner.interface';
 
 export interface Property {
     id?: number;
@@ -22,9 +23,57 @@ export interface Property {
     IPTU?: number;
     value?: number;
     company: Company;
+    owner: Owner;
     administrator?: Administrator;
     manager?: Manager;
     advisor?: Advisor;
     broker?: Broker;
     secretary?: Secretary;
+    createdAt: string;
+    updatedAt: string;
+}
+
+export interface CreateProperty {
+    description?: string;
+    photos?: JSON;
+    checked: boolean;
+    elevator?: boolean;
+    bedrooms?: number;
+    bathrooms?: number;
+    suites?: number;
+    parkingLots?: number;
+    terrainArea?: number;
+    buildingArea?: number;
+    totalUtilTerrainArea?: number;
+    condominium?: number;
+    IPTU?: number;
+    value?: number;
+    owner: Owner;
+    administrator?: Administrator;
+    manager?: Manager;
+    advisor?: Advisor;
+    broker?: Broker;
+    secretary?: Secretary;
+}
+
+export interface UpdateProperty {
+    description?: string;
+    photos?: JSON;
+    checked: boolean;
+    elevator?: boolean;
+    bedrooms?: number;
+    bathrooms?: number;
+    suites?: number;
+    parkingLots?: number;
+    terrainArea?: number;
+    buildingArea?: number;
+    totalUtilTerrainArea?: number;
+    condominium?: number;
+    IPTU?: number;
+    value?: number;
+    owner: Owner;
+}
+
+export interface DeleteProperty {
+    property: number;
 }
