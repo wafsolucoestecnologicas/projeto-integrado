fb69db3d23d0457ba062d0562be7d4400a59c880
Wellington Felix <wellington.felix@fornecedores.sicoob.com.br>
2022-04-19 17:36:26 -0300

    Criando interfaces de comissões a receber.

--- imob-web/front-end/src/app/core/interfaces/commission-receivable.interface.ts
+++ imob-web/front-end/src/app/core/interfaces/commission-receivable.interface.ts
@@ -7,4 +7,25 @@ export interface CommissionReceivable {
     value: number;
     company: Company;
     property: Property;
+    createdAt: string;
+    updatedAt: string;
+}
+
+export interface CreateCommissionReceivable {
+    date: string;
+    value: number;
+    property: Property;
+}
+
+export interface UpdateCommissionReceivable {
+    date: string;
+    value: number;
+}
+
+export interface DeleteCommissionReceivable {
+    commissionReceivable: number;
+}
+
+export interface Receivable {
+    totalValueReceivable: number;
 }
