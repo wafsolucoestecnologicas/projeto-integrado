af5294c849f605c8cdd073f56c8a1ba4fea9f3b7
Wellington Felix <wellington.felix@fornecedores.sicoob.com.br>
2022-04-20 14:45:15 -0300

    Criando interfaces de négocio.

--- imob-web/front-end/src/app/core/interfaces/business.interface.ts
+++ imob-web/front-end/src/app/core/interfaces/business.interface.ts
@@ -6,8 +6,8 @@ import { Broker } from './broker.interface';
 import { Secretary } from './secretary.interface';
 import { Owner } from './owner.interface';
 import { Customer } from './customer.interface';
-import { Property } from './property.interface';
 import { Lead } from './lead.interface';
+import { Property } from './property.interface';
 
 export interface Business {
     id?: number;
@@ -40,6 +40,114 @@ export interface Business {
     secretary?: Secretary;
     owner: Owner;
     customer: Customer;
-    property: Property;
     lead: Lead;
+    property: Property;
+    createdAt: string;
+    updatedAt: string;
+}
+
+export interface CreateBusiness {
+    status: number;
+    dateVisit?: string;
+    dateSale?: string;
+    visitForm?: string;
+    propertyRegistration?: string;
+    propertySaleContract?: string;
+    ITBI?: string;
+    customerRG?: string;
+    customerCPF?: string;
+    customerAddressProof?: string;
+    customerPayslip?: string;
+    ownerRG?: string;
+    ownerCPF?: string;
+    ownerAddressProof?: string;
+    ownerPayslip?: string;
+    createdByAdministrator: boolean;
+    createdByManager: boolean;
+    createdBySecretary: boolean;
+    redirectedManagerId?: number;
+    redirectedAdvisorId?: number;
+    redirectedBrokerId?: number;
+    company: Company;
+    administrator?: Administrator;
+    manager?: Manager;
+    advisor?: Advisor;
+    broker?: Broker;
+    secretary?: Secretary;
+    owner?: Owner;
+    customer?: Customer;
+    lead?: Lead;
+    property?: Property;
+}
+
+export interface UpdateBusiness {
+    status: number;
+    dateVisit?: string;
+    dateSale?: string;
+    visitForm?: string;
+    propertyRegistration?: string;
+    propertySaleContract?: string;
+    ITBI?: string;
+    customerRG?: string;
+    customerCPF?: string;
+    customerAddressProof?: string;
+    customerPayslip?: string;
+    ownerRG?: string;
+    ownerCPF?: string;
+    ownerAddressProof?: string;
+    ownerPayslip?: string;
+    createdByAdministrator: boolean;
+    createdByManager: boolean;
+    createdBySecretary: boolean;
+    redirectedManagerId?: number;
+    redirectedAdvisorId?: number;
+    redirectedBrokerId?: number;
+    administrator?: Administrator;
+    manager?: Manager;
+    advisor?: Advisor;
+    broker?: Broker;
+    secretary?: Secretary;
+    owner?: Owner;
+    customer?: Customer;
+    lead?: Lead;
+    property?: Property;
+}
+
+export interface DeleteBusiness {
+    business: number;
+}
+
+export interface AmountBusiness {
+    totalAmountProspecting: number;
+    totalAmountVisit: number;
+    totalAmountProposal: number;
+    totalAmountRejected: number;
+    totalAmountClosed: number;
+    totalAmountBusinesses: number;
+}
+
+export interface TransferManager {
+    manager: {
+        id: number;
+    };
+}
+
+export interface TransferAdvisor {
+    advisor: {
+        id: number;
+    };
+}
+
+export interface TransferBroker {
+    broker: {
+        id: number;
+    };
+}
+
+export interface RejectBusiness {
+    business: number;
+}
+
+export interface CloseBusiness {
+    business: number;
 }
