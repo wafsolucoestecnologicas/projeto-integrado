d020bf6d30641f7c7fd09fb2e0bbf3e6e15ac4e0
Wellington Felix <wellington.felix@fornecedores.sicoob.com.br>
2022-04-20 14:47:28 -0300

    Refatorando código.

--- imob-web/front-end/src/app/components/users/delete-user/delete-user.component.ts
+++ imob-web/front-end/src/app/components/users/delete-user/delete-user.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit, OnDestroy, Inject } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { Subscription } from 'rxjs';
 
-import { DeleteUser, User } from 'src/app/core/interfaces/user.interface';
+import { User, DeleteUser } from 'src/app/core/interfaces/user.interface';
 import { UserService } from 'src/app/core/services/user.service';
 import { AlertService } from 'src/app/shared/services/alert.service';
 
