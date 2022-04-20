ae2a5f4614baab9f4b51dcf451932d98aa8ec9a1
Wellington Felix <wellington.felix@fornecedores.sicoob.com.br>
2022-04-20 14:45:42 -0300

    Criando enum de rotas de négocio.

---
+++ imob-web/front-end/src/app/core/enums/business.enum.ts
@@ -0,0 +1,10 @@
+export enum BusinessRoutes {
+    BUSINESSES = 'businesses',
+    AMOUNT = 'businesses/amount',
+    TRANSFER_MANAGER = 'businesses/transfer/manager',
+    TRANSFER_ADVISOR = 'businesses/transfer/advisor',
+    TRANSFER_BROKER = 'businesses/transfer/broker',
+    REJECT_BUSINESS = 'businesses/reject',
+    CLOSE_BUSINESS = 'businesses/close',
+    UPLOAD = 'businesses/upload'
+}
